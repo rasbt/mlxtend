7ff0ba34f46045bb6ca1bc9d6ebaec47811d35cf
rasbt <se.raschka@me.com>
2014-08-13 22:23:10 -0400

    rm dist files

--- build/lib/mlxtend/__init__.py
+++
@@ -1,5 +0,0 @@
-# Sebastian Raschka 08/13/2014
-# mlxtend Machine Learning Library Extensions
-
-__version__ = '0.1.0'
-
--- build/lib/mlxtend/sklearn/__init__.py
+++
@@ -1,7 +0,0 @@
-# Sebastian Raschka 08/13/2014
-# mlxtend Machine Learning Library Extensions
-# Submodules with scikit-learn utilities
-
-from .feature_sel import FeatureSelector
-
-

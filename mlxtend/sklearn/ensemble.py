99e3ed2674b388a8aae221335da03d9f1c101cb6
rasbt <se.raschka@me.com>
2015-01-26 22:58:40 -0500

    class label fix

--- mlxtend/sklearn/ensemble.py
+++ mlxtend/sklearn/ensemble.py
@@ -1,57 +1,26 @@
-# Sebastian Raschka 08/13/2014
-# mlxtend Machine Learning Library Extensions
-
 """
 Soft Voting/Majority Rule classifier
 
-This module contains a Soft Voting/Majority Rule ensemble classifier for 
-scikit-learn classification estimators.
+This module contains a Soft Voting/Majority Rule classifier for 
+classification clfs.
 
-## Examples:
-
-Intializing different scikit-learn estimators for classification
->>> clf1 = LogisticRegression()
->>> clf2 = RandomForestClassifier()
->>> clf3 = GaussianNB()
-
-Example 1: Using Majority Class Label
-initialization for using the majority class labels for prediction
->>> eclf = VotingClassifier(clfs=[clf1, clf2, clf3], voting='hard')
->>> eclf.fit(X_train, y_train)
->>> eclf.fit(X_test)
-
-Example 2: Using Average Probabilities
-initialization for using average predict_proba scores 
-to derive the class label.
->>> eclf = VotingClassifier(clfs=[clf1, clf2, clf3], voting='soft')
->>> eclf.fit(X_train, y_train)
->>> eclf.fit(X_test)
-
-Example 3: Using Weighted Average Probabilities
-initialization for multiplying average predict_proba 
-scores to derive the class label.
->>> eclf = VotingClassifier(clfs=[clf1, clf2, clf3], voting='soft', weights=[2,1,5])
->>> eclf.fit(X_train, y_train)
->>> eclf.fit(X_test)
 
 """
-
-
-
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
 from sklearn.base import TransformerMixin
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import operator
 
 class EnsembleClassifier(BaseEstimator, ClassifierMixin, TransformerMixin):
     """ 
-    Soft Voting/Majority Rule ensemble classifier for scikit-learn estimators.
+    Soft Voting/Majority Rule classifier for classification clfs.
         
     Parameters
     ----------
     clfs : array-like, shape = [n_classifiers]
-      A list of scikit-learn classifier objects.
+      A list of clfs for classification.
       
     voting : str, {'hard', 'soft'} (default='hard')
       If 'hard', uses predicted class labels for majority rule voting.
@@ -61,16 +30,40 @@ class EnsembleClassifier(BaseEstimator, ClassifierMixin, TransformerMixin):
     weights : array-like, shape = [n_classifiers], optional (default=`None`)
       Sequence of weights (`float` or `int`) that are multiplied with the 
       predicted class probabilities before averaging if `voting='soft'` 
-      (weights are ignored if `voting='hard'`).
-      Assumes equal weights if `None`.
+      Uses uniform weights if `None`.
         
     Attributes
     ----------
-    classes_ : array-like, shape = [n_class_labels, n_classifiers]
-        Class labels predicted by each classifier if `weights=None`.
+    classes_ : array-like, shape = [n_class_labels]
        
     probas_ : array, shape = [n_probabilities, n_classifiers]
         Predicted probabilities by each classifier if `weights=array-like`.
+        
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn.linear_model import LogisticRegression
+    >>> from sklearn.naive_bayes import GaussianNB 
+    >>> from sklearn.ensemble import RandomForestClassifier
+    >>> np.random.seed(123)
+    >>> clf1 = LogisticRegression()
+    >>> clf2 = RandomForestClassifier()
+    >>> clf3 = GaussianNB()
+    >>> X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+    >>> y = np.array([1, 1, 1, 2, 2, 2])
+    >>> eclf1 = EnsembleClassifier(clfs=[clf1, clf2, clf3], voting='hard')
+    >>> eclf1 = eclf1.fit(X, y)
+    >>> print(eclf1.predict(X))
+    [1 1 1 2 2 2]
+    >>> eclf2 = EnsembleClassifier(clfs=[clf1, clf2, clf3], voting='soft')
+    >>> eclf2 = eclf2.fit(X, y)
+    >>> print(eclf2.predict(X))
+    [1 1 1 2 2 2]
+    >>> eclf3 = EnsembleClassifier(clfs=[clf1, clf2, clf3], voting='soft', weights=[2,1,1])
+    >>> eclf3 = eclf3.fit(X, y)
+    >>> print(eclf3.predict(X))
+    [1 1 1 2 2 2]
+    >>>
       
     """
     def __init__(self, clfs, voting='hard', weights=None):
@@ -86,10 +79,13 @@ class EnsembleClassifier(BaseEstimator, ClassifierMixin, TransformerMixin):
             raise ValueError('Number of classifiers and weights must be equal')      
         
         self.weights = weights
+        self.le = LabelEncoder()
+        self.classes_ = None
+
 
     def fit(self, X, y):
         """ 
-        Fits the scikit-learn estimators.
+        Fits the clfs.
         
         Parameters
         ----------
@@ -107,6 +103,10 @@ class EnsembleClassifier(BaseEstimator, ClassifierMixin, TransformerMixin):
         """
         for clf in self.clfs:
             clf.fit(X, y)
+        
+        self.le.fit(y)
+        self.classes_ = np.unique(y)
+                
         return self
             
     def predict(self, X):
@@ -125,17 +125,25 @@ class EnsembleClassifier(BaseEstimator, ClassifierMixin, TransformerMixin):
         """
         if self.voting == 'soft':
             avg = self.predict_proba(X)
-            maj = np.apply_along_axis(lambda x: max(enumerate(x), key=operator.itemgetter(1))[0], axis=1, arr=avg)
+            maj = self.le.inverse_transform(np.argmax(avg, axis=1))
         
         else: # 'hard' voting
-            self.classes_ = self._get_classes(X)
-            maj = np.asarray([np.argmax(np.bincount(self.classes_[:,c])) for c in range(self.classes_.shape[1])]) 
+            self.classes_ = self._predict(X)
+            
+            # arange class labels as [n_class_labels, n_classifier_predictions]
+            self.classes_ = np.asarray([self.classes_[:,c] for c in range(self.classes_.shape[1])])
+            
+            # duplicate class labels if weights are provided for argmax majority rule
+            if self.weights:
+                self.classes_ = np.concatenate([np.tile(self.classes_[:,c,None], w)
+                                        for w,c in zip(self.weights, range(self.classes_.shape[1]))], axis=1)
+
+            maj = np.apply_along_axis(lambda x: np.argmax(np.bincount(x)), axis=1, arr=self.classes_)
         
         return maj
             
     
     def predict_proba(self, X):
-        
         """
         Parameters
         ----------
@@ -149,7 +157,7 @@ class EnsembleClassifier(BaseEstimator, ClassifierMixin, TransformerMixin):
             Weighted average probability for each class per sample.
         
         """
-        self.probas_ = self._get_probas(X)
+        self.probas_ = self._predict_probas(X)
         avg = np.average(self.probas_, axis=0, weights=self.weights)
         
         return avg
@@ -175,14 +183,18 @@ class EnsembleClassifier(BaseEstimator, ClassifierMixin, TransformerMixin):
         
         """
         if self.weights:
-            return self._get_probas(X)
+            return self._predict_probas(X)
         else:
-            return self._get_classes(X)  
+            return self._predict(X)  
     
-    def _get_classes(self, X):
+    def _predict(self, X):
         """ Collects results from clf.predict calls. """
         return np.asarray([clf.predict(X) for clf in self.clfs])
         
-    def _get_probas(self, X):
+    def _predict_probas(self, X):
         """ Collects results from clf.predict calls. """
         return np.asarray([clf.predict_proba(X) for clf in self.clfs])
+        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()

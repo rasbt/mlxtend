6ec742b793160dc7a7360d8fc79132035a3e64f7
rasbt <mail@sebastianraschka.com>
2015-10-09 19:59:03 -0400

    float ary in test

--- tests/tests_preprocessing/test_mean_centering.py
+++ tests/tests_preprocessing/test_mean_centering.py
@@ -3,12 +3,12 @@ from mlxtend.preprocessing import MeanCenterer
 
 
 def test_mean_centering():
-    X1 = np.array([[1, 2, 3], [4, 5, 6]])
-    X1_out = np.array([[-1, -1, -1], [1,  1,  1]])
+    X1 = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    X1_out = np.array([[-1.0, -1.0, -1.0], [1.0,  1.0,  1.0]])
     mc = MeanCenterer()
     assert(mc.fit_transform(X1).all() == X1_out.all())
 
-    X2 = [1, 2, 3]
-    X2_out = np.array([-1, 0, 1])
+    X2 = [1.0, 2.0, 3.0]
+    X2_out = np.array([-1.0, 0.0, 1.0])
     mc = MeanCenterer()
     assert(mc.fit_transform(X2).all().all() == X2_out.all())

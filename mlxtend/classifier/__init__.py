9f6eb9fe06d65228387f578c264c933b5a8ce647
rasbt <mail@sebastianraschka.com>
2015-10-12 08:36:31 -0400

    0.29 doc

--- mlxtend/classifier/__init__.py
+++
@@ -1,8 +0,0 @@
-# Sebastian Raschka 2015
-# mlxtend Machine Learning Library Extensions
-
-from .perceptron import Perceptron
-from .adaline import Adaline
-from .logistic_regression import LogisticRegression
-from .neuralnet_mlp import NeuralNetMLP
-__all__ = ["Perceptron", "Adaline", "LogisticRegression", "NeuralNetMLP"]

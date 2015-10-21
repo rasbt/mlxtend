3b1fc126ac1ee8ed997da5de72dbb4e3bb7b96a0
rasbt <mail@sebastianraschka.com>
2015-10-21 01:53:27 -0400

    regr. on housing data tests

--- tests/tests_regression/test_linear_regression.py
+++ tests/tests_regression/test_linear_regression.py
@@ -1,67 +1,52 @@
 from mlxtend.regression import LinearRegression
-from mlxtend.data import iris_data
+from mlxtend.data import boston_housing_data
 import numpy as np
 from numpy.testing import assert_almost_equal
 
+X, y = boston_housing_data()
+X_rm = X[:, 5][:,np.newaxis]
+X_rm_lstat = X[:, [5, -1]]
+expect_rm = np.array([-34.671, 9.102])
+expect_rm_lstat = np.array([-1.358, 5.095, -0.642])
+
+# standardized variables
+X_rm_std = (X_rm - X_rm.mean(axis=0)) / X_rm.std(axis=0)
+X_rm_lstat_std = (X_rm_lstat - X_rm_lstat.mean(axis=0)) / X_rm_lstat.std(axis=0)
+y_std = (y - y.mean()) / y.std()
+expect_rm_std = np.array([0.000, 0.695])
+expect_rm_lstat_std = np.array([0.000, 0.389, -0.499])
+
 def test_univariate_normal_equation():
-    X = np.array([ 1, 2, 3, 4, 5])[:, np.newaxis]
-    y = np.array([ 1, 2, 3, 4, 5])
     ne_lr = LinearRegression(solver='normal_equation')
-    ne_lr.fit(X, y)
-    assert_almost_equal(ne_lr.w_, np.array([0.00, 1.00]), decimal=7)
-    
+    ne_lr.fit(X_rm, y)
+    assert_almost_equal(ne_lr.w_, expect_rm, decimal=3)
+
+def test_univariate_normal_equation_std():
+    ne_lr = LinearRegression(solver='normal_equation')
+    ne_lr.fit(X_rm_std, y_std)
+    assert_almost_equal(ne_lr.w_, expect_rm_std, decimal=3)
+
 def test_univariate_gradient_descent():
-    X = np.array([ 1, 2, 3, 4, 5])[:, np.newaxis]
-    y = np.array([ 1, 2, 3, 4, 5])
-    X_std = (X - np.mean(X)) / X.std()
-    y_std = (y - np.mean(y)) / y.std()
-    
-    gd_lr = LinearRegression(solver='gd', eta=0.1, epochs=50, random_seed=0)
-    gd_lr.fit(X_std, y_std)
-    assert_almost_equal(gd_lr.w_, np.array([0.00, 1.00]), decimal=2)
+    gd_lr = LinearRegression(solver='gd', eta=0.001, epochs=500, random_seed=0)
+    gd_lr.fit(X_rm_std, y_std)
+    assert_almost_equal(gd_lr.w_, expect_rm_std, decimal=3)
 
 def test_univariate_stochastic_gradient_descent():
-    X = np.array([ 1, 2, 3, 4, 5])[:, np.newaxis]
-    y = np.array([ 1, 2, 3, 4, 5])
-    X_std = (X - np.mean(X)) / X.std()
-    y_std = (y - np.mean(y)) / y.std()
-    
-    sgd_lr = LinearRegression(solver='sgd', eta=0.1, epochs=10, random_seed=0)
-    sgd_lr.fit(X_std, y_std)
-    assert_almost_equal(sgd_lr.w_, np.array([0.00, 1.00]), decimal=2)
-
+    sgd_lr = LinearRegression(solver='sgd', eta=0.0001, epochs=100, random_seed=0)
+    sgd_lr.fit(X_rm_std, y_std)
+    assert_almost_equal(sgd_lr.w_, expect_rm_std, decimal=2)
 
 def test_multivariate_normal_equation():
-    X = np.array([[1, 2], [2, 3], [4, 5], [6, 7], [7, 8]])
-    y = np.array([ 1, 2, 3, 4, 5])
-    X_std = (X - np.mean(X)) / X.std()
-    y_std = (y - np.mean(y)) / y.std()
     ne_lr = LinearRegression(solver='normal_equation')
-    ne_lr.fit(X_std, y_std)
-    assert_almost_equal(ne_lr.predict(X_std), np.array(
-            [ -1.3054, -0.87029, 0.0000, 0.8703, 1.3054]), 
-            decimal=4)
-    
-def test_multivariate_gradient_descent():
-    X = np.array([[1, 2], [2, 3], [4, 5], [6, 7], [7, 8]])
-    y = np.array([ 1, 2, 3, 4, 5])
-    X_std = (X - np.mean(X)) / X.std()
-    y_std = (y - np.mean(y)) / y.std()
+    ne_lr.fit(X_rm_lstat, y)
+    assert_almost_equal(ne_lr.w_, expect_rm_lstat, decimal=3)
 
-    gd_lr = LinearRegression(solver='gd', eta=0.01, epochs=100, random_seed=0)
-    gd_lr.fit(X_std, y_std)
-    assert_almost_equal(gd_lr.predict(X_std), np.array(
-            [ -1.3054, -0.87029, 0.0000, 0.8703, 1.3054]), 
-            decimal=2)
+def test_multivariate_gradient_descent():
+    gd_lr = LinearRegression(eta=0.001, epochs=500, solver='gd', random_seed=0)
+    gd_lr.fit(X_rm_lstat_std, y_std)
+    assert_almost_equal(gd_lr.w_, expect_rm_lstat_std, decimal=3)
 
 def test_multivariate_stochastic_gradient_descent():
-    X = np.array([[1, 2], [2, 3], [4, 5], [6, 7], [7, 8]])
-    y = np.array([ 1, 2, 3, 4, 5])
-    X_std = (X - np.mean(X)) / X.std()
-    y_std = (y - np.mean(y)) / y.std()
-    
-    sgd_lr = LinearRegression(solver='sgd', eta=0.001, epochs=1000, random_seed=0)
-    sgd_lr.fit(X_std, y_std)
-    assert_almost_equal(sgd_lr.predict(X_std), np.array(
-            [ -1.3054, -0.87029, 0.0000, 0.8703, 1.3054]), 
-            decimal=2)
+    sgd_lr = LinearRegression(eta=0.0001, epochs=500, solver='sgd', random_seed=0)
+    sgd_lr.fit(X_rm_lstat_std, y_std)
+    assert_almost_equal(sgd_lr.w_, expect_rm_lstat_std, decimal=2)

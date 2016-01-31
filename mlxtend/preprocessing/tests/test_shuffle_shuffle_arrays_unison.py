433c169bace6334c215b891455c58f4a4bcfc6fa
rasbt <mail@sebastianraschka.com>
2016-01-31 15:33:24 -0500

    consistent random seed nomencl.

--- mlxtend/preprocessing/tests/test_shuffle_shuffle_arrays_unison.py
+++ mlxtend/preprocessing/tests/test_shuffle_shuffle_arrays_unison.py
@@ -13,7 +13,7 @@ def test_shuffle_arrays_unison():
     X1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     y1 = np.array([1, 2, 3])
 
-    X2, y2 = shuffle_arrays_unison(arrays=[X1, y1], random_state=3)
+    X2, y2 = shuffle_arrays_unison(arrays=[X1, y1], random_seed=3)
 
     assert(X2.all() == np.array([[4, 5, 6], [1, 2, 3], [7, 8, 9]]).all())
     assert(y2.all() == np.array([2, 1, 3]).all())

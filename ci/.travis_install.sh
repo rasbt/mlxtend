6bc29c624e4c67ca5d99cd02b84b026c54e03599
rasbt <mail@sebastianraschka.com>
2016-04-17 01:14:57 -0400

    miniconda update

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -21,11 +21,12 @@ deactivate
 
 # Use the miniconda installer for faster download / install of conda
 # itself
-- if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-      wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-    else
-      wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-    fi
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+
+else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
 
 chmod +x miniconda.sh && ./miniconda.sh -b
 export PATH=/home/travis/miniconda/bin:$PATH

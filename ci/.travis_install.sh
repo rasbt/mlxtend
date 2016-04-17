9c9b55889079f707fe280091288b0a179175d4f7
rasbt <mail@sebastianraschka.com>
2016-04-17 00:59:07 -0400

    new gcc2

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -59,7 +59,7 @@ if [[ "$TENSORFLOW" == "true" ]]; then
       "linux")
           case "$PYTHON_VERSION" in
               "2.7")
-                  pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0rc0-cp27-none-linux_x86_64.whl
+                  pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
               ;;
               "3.4")
                   pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp34-none-linux_x86_64.whl

b2bae0d45c3ec4973ebf3d735d65ca0b39135c58
BatuhanBardak <bbardak37@gmail.com>
2016-12-19 23:58:09 +0300

    added new test on unittest

--- mlxtend/evaluate/tests/test_lift_score.py
+++ mlxtend/evaluate/tests/test_lift_score.py
@@ -6,7 +6,7 @@
 
 from mlxtend.evaluate import lift_score
 from numpy.testing import assert_array_equal
-
+import numpy as np
 
 def test_multiclass():
     y_targ = [1, 1, 1, 0, 0, 2, 0, 3]
@@ -15,10 +15,27 @@ def test_multiclass():
     y = lift_score(y_targ, y_pred, binary=True, positive_label=0)
     assert_array_equal(x, y)
 
+def test_binary_with_numpy():
 
-def test_binary():
-    y_targ = [1, 1, 1, 0, 0, 1, 0, 1]
-    y_pred = [1, 0, 1, 0, 0, 1, 1, 1]
-    x = 1.28
+    y_targ = np.array([1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0])
+    y_pred = np.array([1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0])
+    x = 1.25
     y = lift_score(y_targ, y_pred, binary=False, positive_label=0)
     assert_array_equal(x, y)
+
+
+def test_binary():
+    y_targ = [1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0]
+    y_pred = [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0]
+    x = 1.25
+    y = lift_score(y_targ, y_pred, binary=False, positive_label=0)
+    assert_array_equal(x, y)
+
+
+def test_multidimension():
+    y_targ = [[1, 1, 1, 0, 0, 1],[0, 1, 0, 0, 0, 1]]
+    y_pred = [[1, 0, 1, 0, 0, 1]]
+    x = 1
+    y = lift_score(y_targ, y_pred, binary=False, positive_label=0)
+    assert_array_equal(x, y)
+

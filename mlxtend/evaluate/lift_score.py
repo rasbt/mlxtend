75a7705fcd2d391b35a1c30380818c463810c2d2
BatuhanBardak <bbardak37@gmail.com>
2016-12-20 19:42:32 +0300

    additional unit tests are added, positive label is changed

--- mlxtend/evaluate/lift_score.py
+++ mlxtend/evaluate/lift_score.py
@@ -9,7 +9,7 @@
 import numpy as np
 
 
-def lift_score(y_target, y_predicted, binary=True, positive_label=0):
+def lift_score(y_target, y_predicted, binary=True, positive_label=1):
     """Compute a lift score.
 
     Parameters
@@ -31,6 +31,7 @@ def lift_score(y_target, y_predicted, binary=True, positive_label=0):
     mat : array-like, shape=[n_classes, n_classes]
 
     """
+
     if not isinstance(y_target, np.ndarray):
         targ_tmp = np.asarray(y_target)
     else:
@@ -45,14 +46,36 @@ def lift_score(y_target, y_predicted, binary=True, positive_label=0):
 
     if len(pred_tmp) != len(targ_tmp):
         raise AttributeError('`y_target` and `y_predicted`'
-                             ' don\'t have the same number of elements.')
+                             'don\'t have the same number of elements.')
     if binary:
-        targ_tmp = np.where(targ_tmp != positive_label, 1, 0)
-        pred_tmp = np.where(pred_tmp != positive_label, 1, 0)
+        targ_tmp = np.where(targ_tmp != positive_label, 0, 1)
+        pred_tmp = np.where(pred_tmp != positive_label, 0, 1)
+
+    binary_check_targ_tmp = np.extract(targ_tmp > 1, targ_tmp)
+    binary_check_pred_tmp = np.extract(pred_tmp > 1, pred_tmp)
+
+    if len(binary_check_targ_tmp) or len(binary_check_pred_tmp):
+        raise AttributeError('`y_target` and `y_predicted`'
+                             ' have different elements from 0 and 1.')
+
     return (support(targ_tmp, pred_tmp)/(support(targ_tmp)*support(pred_tmp)))
 
 
 def support(y_target, y_predicted=None):
+    """Compute a support score.
+
+    Parameters
+    -----------
+    y_target : array-like, shape=[n_samples]
+        True class labels.
+    y_predicted : array-like, shape=[n_samples]
+        Predicted class labels.
+
+    Returns
+    ----------
+    score : float
+
+    """
     if y_predicted is None:
         if y_target.ndim == 1:
             return (y_target == 1).sum() / float(y_target.shape[0])

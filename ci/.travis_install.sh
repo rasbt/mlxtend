928f17fcfe98e3998d3735143d3e771221bb4162
rasbt <mail@sebastianraschka.com>
2017-01-23 02:57:30 -0500

    update tensorflow variable initialization to new api

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -36,7 +36,7 @@ python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
 if [ "${TENSORFLOW}" = "true" ]; then
     #conda install -c conda-forge tensorflow
-    export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp35-cp35m-linux_x86_64.whl
+    export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.1-cp35-cp35m-linux_x86_64.whl
     sudo pip install --ignore-installed --upgrade $TF_BINARY_URL
     python -c "import tensorflow; print('tensorflow %s' % tensorflow.__version__)";
 else

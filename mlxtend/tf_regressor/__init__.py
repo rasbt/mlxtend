75420c7902501cf7302e8a8c12d596c11dddf77e
rasbt <mail@sebastianraschka.com>
2017-05-06 16:51:48 -0400

    rm tf

--- mlxtend/tf_regressor/__init__.py
+++
@@ -1,9 +0,0 @@
-# Sebastian Raschka 2014-2017
-# mlxtend Machine Learning Library Extensions
-# Author: Sebastian Raschka <sebastianraschka.com>
-#
-# License: BSD 3 clause
-
-from .tf_linear_regression import TfLinearRegression
-
-__all__ = ["TfLinearRegression"]

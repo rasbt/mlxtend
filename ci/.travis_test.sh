b504d3595f466fe509ac09fc4e4c4a015639b2a1
Sebastian Raschka <mail@sebastianraschka.com>
2017-06-24 01:46:18 -0400

    re-enable plotting tests (#209)

--- ci/.travis_test.sh
+++ ci/.travis_test.sh
@@ -4,9 +4,7 @@ set -e
 
 
 if [[ "$COVERAGE" == "true" ]]; then
-    nosetests -s -v --with-coverage --exclude-dir=mlxtend/tf_classifier --exclude-dir=mlxtend/tf_regressor --exclude-dir=mlxtend/tf_cluster --exclude-dir=mlxtend/plotting
-else
-    nosetests -s -v --exclude-dir=mlxtend/tf_classifier --exclude-dir=mlxtend/tf_regressor --exclude-dir=mlxtend/tf_cluster --exclude-dir=mlxtend/plotting
+    nosetests -s -v --with-coverage
 fi
 
 if [[ "$NOTEBOOKS" == "true" ]]; then

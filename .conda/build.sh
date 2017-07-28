a68185ec633fcb3301b152fe6467d55fcb7e931b
Sebastian Raschka <mail@sebastianraschka.com>
2017-07-28 16:23:03 -0400

    update setup.py install requirements (#223)

--- .conda/build.sh
+++ .conda/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-$PYTHON setup.py install
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+$PYTHON setup.py install --single-version-externally-managed --record record.txt

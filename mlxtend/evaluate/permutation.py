183c0de55d97fb4e0b36b6e5ccdf7211c2074dce
rasbt <mail@sebastianraschka.com>
2017-09-14 17:04:01 -0400

    permutation test docs

--- mlxtend/evaluate/permutation.py
+++ mlxtend/evaluate/permutation.py
@@ -14,7 +14,7 @@ from nose.tools import nottest
 # decorator to prevent nose to consider
 # this as a unit test due to "test" in the name
 @nottest
-def permutation_test(x, y, alt_hypothesis='x > y', method='exact',
+def permutation_test(x, y, alt_hypothesis='x != y', method='exact',
                      num_permutations=1000, seed=None):
     """
     Nonparametric permutation test
@@ -27,10 +27,10 @@ def permutation_test(x, y, alt_hypothesis='x > y', method='exact',
     y : list or numpy array with shape (n_datapoints,)
         A list or 1D numpy array of the second sample
         (e.g., the control group).
-    alt_hypothesis : str
+    alt_hypothesis : str (default='x != y')
         The alternativ hypothesis. It 'x > y' or 'x < y',
         a one-sided permutation test is performerd to test, and
-        a two-sided permutation test is performed if 'x != y',
+        a two-sided permutation test is performed if 'x != y' (default),
         to test the null hypothesis that both x and y are samples
         from the same population.
     method : 'approximate' or 'exact' (default: 'exact')
@@ -40,7 +40,7 @@ def permutation_test(x, y, alt_hypothesis='x > y', method='exact',
         Note that 'exact' is typically not feasible unless the dataset
         size is relatively small.
     num_permutations : int (default: 1000)
-        The number of permutation samples if method='approximate'.
+        The number of permutation samples if `method='approximate'`.
     seed : int or None (default: None)
         The random seed for generating permutation samples if
         `method='approximate'`.
@@ -57,6 +57,12 @@ def permutation_test(x, y, alt_hypothesis='x > y', method='exact',
     if method not in ('approximate', 'exact'):
         raise AttributeError('method must be "approximate"'
                              ' or "exact", got %s' % method)
+
+    if isinstance(x, np.ndarray) and len(x.shape) != 1:
+        raise AttributeError('x must be one-dimensional')
+
+    if isinstance(y, np.ndarray) and len(y.shape) != 1:
+        raise AttributeError('y must be one-dimensional')
 
     def one_sided_statistic(comb, x):
         # equivalent to np.mean(x) - np.mean(y)
--- mlxtend/evaluate/tests/test_permutation.py
+++ mlxtend/evaluate/tests/test_permutation.py
@@ -13,13 +13,15 @@ treatment = [689, 656, 668, 660, 679, 663, 664, 647]
 control = [657, 623, 652, 654, 658, 660, 670, 620]
 
 
-def test_defaults():
-    p = permutation_test(treatment, control)
+def test_one_sided():
+    p = permutation_test(treatment, control, alt_hypothesis='x > y')
     assert round(p, 4) == 0.0274
 
 
 def test_numpy_array():
-    p = permutation_test(np.array(treatment), np.array(control))
+    p = permutation_test(np.array(treatment),
+                         np.array(control),
+                         alt_hypothesis='x > y')
     assert round(p, 4) == 0.0274
 
 
@@ -37,10 +39,17 @@ def test_two_sided():
     assert round(p, 2) == round(2*0.027, 2)
 
 
+def test_defaults():
+    p = permutation_test(treatment,
+                         control)
+    assert round(p, 2) == round(2*0.027, 2)
+
+
 def test_approximate():
     p = permutation_test(treatment,
                          control,
                          method='approximate',
+                         alt_hypothesis='x > y',
                          num_permutations=5000,
                          seed=123)
     assert round(p, 3) == 0.028, round(p, 4)
@@ -65,3 +74,21 @@ def test_invalid_method():
                   [3, 4, 5],
                   alt_hypothesis='x > y',
                   method='na')
+
+
+def test_invalid_array_dim_x():
+    msg = 'x must be one-dimensional'
+    assert_raises(AttributeError,
+                  msg,
+                  permutation_test,
+                  np.array([[1, 2, 3]]),
+                  [3, 4, 5])
+
+
+def test_invalid_array_dim_y():
+    msg = 'y must be one-dimensional'
+    assert_raises(AttributeError,
+                  msg,
+                  permutation_test,
+                  [3, 4, 5],
+                  np.array([[1, 2, 3]]),)

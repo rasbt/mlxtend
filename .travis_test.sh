5cc7aca23841fa511083e2ab6ae9c8114078f9cd
rasbt <mail@sebastianraschka.com>
2017-10-18 01:56:43 -0400

    fix travis ci for py27

---
+++ .travis_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+if [[ "$COVERAGE" == "true" ]]; then
+    nosetests -s -v --with-coverage
+else
+	nosetests -s -v
+fi
+
+if [[ "$NOTEBOOKS" == "true" ]]; then
+    cd docs
+    python make_api.py
+    find sources -name "*.ipynb" -exec jupyter nbconvert --to notebook --execute {} \;
+fi

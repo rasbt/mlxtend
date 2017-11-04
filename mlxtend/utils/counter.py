694e2607f941c1e466013ae28cb2dc7f8b21f54a
Mathew Savage <mathew@mathewsavage.com>
2017-11-04 15:50:39 +0000

    Update counter.py

--- mlxtend/utils/counter.py
+++ mlxtend/utils/counter.py
@@ -1,18 +1,7 @@
-# Sebastian Raschka 2014-2017
-# mlxtend Machine Learning Library Extensions
-#
-# A counter class for printing the progress of an iterator.
-# Author: Sebastian Raschka <sebastianraschka.com>
-#
-# License: BSD 3 clause
-
-import time
-import sys
-
-
 class Counter(object):
 
     """Class to display the progress of for-loop iterators.
+
     Parameters
     ----------
     stderr : bool (default: True)
@@ -25,18 +14,32 @@ class Counter(object):
     name : string (default: None)
         Prepends the specified name before the counter to allow distinguishing
         between multiple counters.
+
     Attributes
     ----------
     curr_iter : int
         The current iteration.
     start_time : float
         The system's time in seconds when the Counter was initialized.
-    iteration_time : float
+    end_time : float
         The system's time in seconds when the Counter was last updated.
-    total_elapsed : float
-        Time in seconds since the Counter was initialized.
-    iteration_elapsed : float
-        Time in seconds since the Counter was last updated.
+
+    Examples
+    --------
+    >>> cnt = Counter()
+    >>> for i in range(20):
+    ...     # do some computation
+    ...     time.sleep(0.1)
+    ...     cnt.update()
+
+    20 iter | 2 sec
+    >>> print('The counter was initialized.'
+              ' %d seconds ago.' % (time.time() - cnt.start_time))
+    The counter was initialized 2 seconds ago
+    >>> print('The counter was last updated'
+              ' %d seconds ago.' % (time.time() - cnt.end_time))
+    The counter was last updated 0 seconds ago.
+
     """
     def __init__(self, stderr=False, start_newline=True, precision=0,
                  name=None):
@@ -50,7 +53,6 @@ class Counter(object):
             self.precision = '%d'
         self.name = name
         self.start_time = time.time()
-        self.iteration_time = time.time()
         self.curr_iter = 0
         if start_newline:
             self.stream.write('\n')
@@ -58,19 +60,14 @@ class Counter(object):
     def update(self):
         """Print current iteration and time elapsed."""
         self.curr_iter += 1
-        self.iteration_time = time.time()
+
+        self.end_time = time.time()
+
         out = '%d iter | %s sec' % (self.curr_iter,
-                                    self.precision % self.total_elapsed())
+                                    self.precision % (self.end_time
+                                                      - self.start_time))
         if self.name is None:
             self.stream.write('\r%s' % out)
         else:
             self.stream.write('\r %s: %s' % (self.name, out))
         self.stream.flush()
-
-    def total_elapsed(self):
-        """Return time elapsed since initialized."""
-        return time.time() - self.start_time
-
-    def iteration_elapsed(self):
-        """Return time elapsed since last updated."""
-        return time.time() - self.iteration_time

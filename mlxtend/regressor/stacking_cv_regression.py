08e58b21153df0e26bc944368f187b937fcd4c3a
selay01 <selay01@users.noreply.github.com>
2018-05-20 15:34:13 +0800

    bug fix when using refit=False
    bug fix when using refit=False

--- mlxtend/regressor/stacking_cv_regression.py
+++ mlxtend/regressor/stacking_cv_regression.py
@@ -140,8 +140,8 @@ class StackingCVRegressor(BaseEstimator, RegressorMixin, TransformerMixin):
             self.regr_ = [clone(clf) for clf in self.regressors]
             self.meta_regr_ = clone(self.meta_regressor)
         else:
-            self.clfs_ = self.regressors
-            self.meta_clf_ = self.meta_regressor
+            self.regr_ = self.regressors
+            self.meta_regr_ = self.meta_regressor
 
         kfold = check_cv(self.cv, y)
         if isinstance(self.cv, int):

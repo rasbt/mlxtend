cd4195b55f7127533e1dd2e1e7c7505243dfdfae
JJLWHarrison <jjharrison89@gmail.com>
2018-06-29 12:35:59 +1000

    Update based on suggestions from issue #402
    * Add ability to show normalised confusion matrix coefficients.
    * Add ability to show colorbar
    * Change to use full range of colormap
    * Set default size based on number of classes.

--- mlxtend/plotting/plot_confusion_matrix.py
+++ mlxtend/plotting/plot_confusion_matrix.py
@@ -1,3 +1,7 @@
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
 # Sebastian Raschka 2014-2018
 # mlxtend Machine Learning Library Extensions
 # Author: Sebastian Raschka <sebastianraschka.com>
@@ -9,11 +13,14 @@ import matplotlib.pyplot as plt
 
 
 def plot_confusion_matrix(conf_mat,
-                          hide_spines=False, hide_ticks=False,
-                          figsize=(2.5, 2.5),
-                          cmap=None, alpha=0.3):
+                          hide_spines=False,
+                          hide_ticks=False,
+                          figsize=None,
+                          cmap=None,
+                          colorbar=False,
+                          show_absolute=True,
+                          show_normed=False):
     """Plot a confusion matrix via matplotlib.
-
     Parameters
     -----------
     conf_mat : array-like, shape = [n_classes, n_classes]
@@ -26,29 +33,63 @@ def plot_confusion_matrix(conf_mat,
         Height and width of the figure
     cmap : matplotlib colormap (default: `None`)
         Uses matplotlib.pyplot.cm.Blues if `None`
-
+    colorbar : bool (default: False)
+        Shows a colorbar if True
+    show_absolute : bool (default: True)
+        Shows absolute confusion matrix coefficients if True.
+        At least one of  ``show_absolute`` or ``show_normed``
+        must be True.
+    show_normed : bool (default: False)
+        Shows normed confusion matrix coefficients if True.
+        At least one of  ``show_absolute`` or ``show_normed``
+        must be True.
     Returns
     -----------
     fig, ax : matplotlib.pyplot subplot objects
         Figure and axis elements of the subplot.
-
     Examples
     -----------
     For usage examples, please see
     http://rasbt.github.io/mlxtend/user_guide/plotting/plot_confusion_matrix/
-
     """
+    if not (show_absolute or show_normed):
+        raise AssertionError('Both show_absolute and show_normed are False')
+
+    total_samples = conf_mat.sum(axis=1)[:, np.newaxis]
+    normed_conf_mat = conf_mat.astype('float') / total_samples
+
     fig, ax = plt.subplots(figsize=figsize)
+    ax.grid(False)
     if cmap is None:
         cmap = plt.cm.Blues
-    ax.matshow(conf_mat, cmap=cmap, alpha=alpha)
+
+    if figsize is None:
+        figsize = (len(conf_mat)*1.25, len(conf_mat)*1.25)
+
+    if show_absolute:
+        matshow = ax.matshow(conf_mat, cmap=cmap)
+    else:
+        matshow = ax.matshow(normed_conf_mat, cmap=cmap)
+
+    if colorbar:
+        fig.colorbar(matshow)
+
     for i in range(conf_mat.shape[0]):
         for j in range(conf_mat.shape[1]):
+            cell_text = ""
+            if show_absolute:
+                cell_text += format(conf_mat[i, j], 'd')
+                if show_normed:
+                    cell_text += "\n" + '('
+                    cell_text += format(normed_conf_mat[i, j], '.2f') + ')'
+            else:
+                cell_text += format(normed_conf_mat[i, j], '.2f')
             ax.text(x=j,
                     y=i,
-                    s=conf_mat[i, j],
+                    s=cell_text,
                     va='center',
-                    ha='center')
+                    ha='center',
+                    color="white" if normed_conf_mat[i, j] > 0.5 else "black")
 
     if hide_spines:
         ax.spines['right'].set_visible(False)

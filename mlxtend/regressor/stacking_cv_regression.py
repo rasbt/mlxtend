e871768b28dc5493a65bb069b088789b78387832
Kota Mori <kmori05@gmail.com>
2018-09-23 21:01:44 -0500

    Add test for cv regressor and docstring

--- mlxtend/regressor/stacking_cv_regression.py
+++ mlxtend/regressor/stacking_cv_regression.py
@@ -133,8 +133,10 @@ class StackingCVRegressor(BaseEstimator, RegressorMixin, TransformerMixin):
             folding strategies such as GroupKFold()
 
         sample_weight : array-like, shape = [n_samples], optional
-            Sample weights. Required that all regressors and 
-            meta_regressors has fit() method with sample_weight option.
+            Sample weights passed as sample_weights to 
+            each regressor in the regressors list as well as the meta_regressor.
+            Raises error if some regressor does not support 
+            sample_weight in the fit() method.
 
         Returns
         -------

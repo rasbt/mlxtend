bd24550b6fce0242b6d3d241cc83c02ed56193f9
vmirly <mirjalil@msu.edu>
2018-11-03 21:17:34 -0400

    Added a test function for extract_face_landmarks()

--- mlxtend/image/tests/test_extract_face_landmarks.py
+++ mlxtend/image/tests/test_extract_face_landmarks.py
@@ -5,7 +5,19 @@
 # License: BSD 3 clause
 
 from mlxtend.image import extract_face_landmarks
+import imageio
+import numpy as np
 
 
 def test_defaults():
-    extract_face_landmarks()
+    img = imageio.imread('data/lena.jpg')
+    landmarks = extract_face_landmarks(img)
+    assert landmarks.shape == (68, 2)
+
+    true_vals = np.array([[312, 376],
+                          [285, 247],
+                          [316, 319],
+                          [274, 274],
+                          [298, 345],
+                          [272, 352]], dtype=np.int32)
+    assert np.all(landmarks[np.arange(10, 70, 10)] == true_vals)

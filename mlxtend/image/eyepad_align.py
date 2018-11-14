5500f6a08d2f07861a700108a0c2a85567a5ff0f
vmirly <mirjalil@msu.edu>
2018-11-14 11:11:49 -0500

    Adding a new module for aligning face images across different datasets

---
+++ mlxtend/image/eyepad_align.py
@@ -0,0 +1,19 @@
+# Sebastian Raschka 2014-2018
+# contributor: Vahid Mirjalili
+# mlxtend Machine Learning Library Extensions
+#
+# A counter class for printing the progress of an iterator.
+# Author: Sebastian Raschka <sebastianraschka.com>
+#
+# License: BSD 3 clause
+
+
+class EyepadAlign():
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass

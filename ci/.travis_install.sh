6b9965f34d0037e40b9aff7024765636cc024237
rasbt <mail@sebastianraschka.com>
2018-11-17 21:10:12 -0600

    workaround since for some reason travis does not support python on osx

--- ci/.travis_install.sh
+++ ci/.travis_install.sh
@@ -2,11 +2,22 @@
 
 set -e
 
-if [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]; then
-    wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then 
+    if [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]; then
+
+        wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+    else
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    fi
 else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    if [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]; then
+
+        wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh
+    else
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+    fi
 fi
+    
 
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"

1ca3059b98f8af0816488c1ac64f86506054ba68
tetrar124 <36783730+tetrar124@users.noreply.github.com>
2019-02-13 17:32:36 +0900

    Applying to iloc of pandas data frame (#500)
    * bug fix and applying to the index of pandas
    
    https://github.com/rasbt/mlxtend/issues/499
    Bug fix for selecting columns from the list and it works not only a list but the index of pandas.
    
    * add error message and  comment
    
    add error message and comment for using it
    
    * add error message and comment
    
    add error message and comment for using it
    
    * Update for pandas iloc
    
    Update for pandas iloc
    
    * Update for pandas iloc
    
    Update for pandas iloc
    
    * comment
    
    comment
    
    * if to elif
    
    for coverage
    
    * elif to if 
    
    elif to if
    
    * For PEP8
    
    only new lines in the parenthesis
    
    * add unit test and changelog note

--- mlxtend/feature_selection/tests/test_column_selector.py
+++ mlxtend/feature_selection/tests/test_column_selector.py
@@ -63,6 +63,18 @@ def test_ColumnSelector_with_dataframe():
     assert df_out.shape == (506, 2)
 
 
+def test_ColumnSelector_with_dataframe_and_int_columns():
+    boston = datasets.load_boston()
+    df_in = pd.DataFrame(boston.data, columns=boston.feature_names)
+    df_out_str = ColumnSelector(cols=('INDUS', 'CHAS')).transform(df_in)
+    df_out_int = ColumnSelector(cols=(2, 3)).transform(df_in)
+
+    np.testing.assert_array_equal(df_out_str[:, 0],
+                                  df_out_int[:, 0])
+    np.testing.assert_array_equal(df_out_str[:, 1],
+                                  df_out_int[:, 1])
+
+
 def test_ColumnSelector_with_dataframe_drop_axis():
     boston = datasets.load_boston()
     df_in = pd.DataFrame(boston.data, columns=boston.feature_names)

92753e0944f9b64bbaefebfcfe67ae03a0855fe4
Janpreet Singh <janpreetsingh.1@gmail.com>
2019-05-17 12:29:35 +0530

    adding version to iris dataset (#539)
    * adding version to iris dataset
    
    * adding version to iris dataset
    
    * adding test cases for iris dataset
    
    * adding test cases for iris dataset
    
    * some minor changes

--- mlxtend/data/iris.py
+++ mlxtend/data/iris.py
@@ -13,7 +13,7 @@ this_dir, this_filename = os.path.split(__file__)
 DATA_PATH = os.path.join(this_dir, "data", "iris.csv.gz")
 
 
-def iris_data():
+def iris_data(version='uci'):
     """Iris flower dataset.
 
     Source : https://archive.ics.uci.edu/ml/datasets/Iris
@@ -28,6 +28,16 @@ def iris_data():
         - 3) petal length [cm]
         - 4) petal width [cm]
 
+
+    Parameters
+    --------
+    version : string, optional (default: 'uci').
+      Version to use {'uci', 'corrected'}. 'uci' loads the dataset
+      as deposited on the UCI machine learning repository, and 
+      'corrected' provides the version that is consistent with
+      Fisher's original paper. See Note for details.
+
+
     Returns
     --------
     X, y : [n_samples, n_features], [n_class_labels]
@@ -41,10 +51,12 @@ def iris_data():
     --------
     The Iris dataset (originally collected by Edgar Anderson) and
     available in UCI's machine learning repository is different from
-    the Iris dataset available in R(and the one in the original paper
-    by R.A. Fisher [1]). Precisely, there are two data points(row number
+    the Iris dataset described in the original paper by  R.A. Fisher [1]). 
+    Precisely, there are two data points (row number
     34 and 37) in UCI's Machine Learning repository are different from the
-    Iris dataset in R (and the one in the original Fisher paper).
+    origianlly published Iris dataset. Also, the original version of the Iris
+    Dataset, which can be loaded via `version='corrected'` is the same
+    as the one in R.
 
     [1] . A. Fisher (1936). "The use of multiple measurements in taxonomic
     problems". Annals of Eugenics. 7 (2): 179–188
@@ -55,9 +67,16 @@ def iris_data():
     http://rasbt.github.io/mlxtend/user_guide/data/iris_data/
 
     """
-    tmp = np.genfromtxt(fname=DATA_PATH, delimiter=',')
-    X, y = tmp[:, :-1], tmp[:, -1]
-    y = y.astype(int)
-    return X, y
-
+    if version == "uci":
+        tmp = np.genfromtxt(fname=DATA_PATH, delimiter=',')
+        X, y = tmp[:, :-1], tmp[:, -1]
+        y = y.astype(int)
+    elif version == "corrected":
+        tmp = np.genfromtxt(fname=DATA_PATH, delimiter=',')
+        X, y = tmp[:, :-1], tmp[:, -1]
+        X[34] = [4.9, 3.1, 1.5, 0.2]
+        X[37] = [4.9, 3.6, 1.4, 0.1]
+        y = y.astype(int)
+    else:
+        raise ValueError("version must be 'uci' or 'corrected'.")
     return X, y
---
+++ mlxtend/data/tests/test_iris.py
@@ -0,0 +1,34 @@
+from mlxtend.data import iris_data
+import numpy as np
+import os
+import re
+from numpy.testing import assert_array_equal
+from nose.tools import assert_raises
+
+
+this_dir, this_filename = os.path.split(__file__)
+this_dir = re.sub("^stset", "", this_dir[::-1])[::-1]
+DATA_PATH = os.path.join(this_dir, "data", "iris.csv.gz")
+
+
+def test_iris_data_uci():
+    tmp = np.genfromtxt(fname=DATA_PATH, delimiter=',')
+    original_uci_data_x, original_uci_data_y = tmp[:, :-1], tmp[:, -1]
+    original_uci_data_y = original_uci_data_y.astype(int)
+    iris_x, iris_y = iris_data()
+    assert_array_equal(original_uci_data_x, iris_x)
+    assert_array_equal(original_uci_data_y, iris_y)
+
+
+def test_iris_data_r():
+    tmp = np.genfromtxt(fname=DATA_PATH, delimiter=',')
+    original_r_data_x, original_r_data_y = tmp[:, :-1], tmp[:, -1]
+    original_r_data_y = original_r_data_y.astype(int)
+    original_r_data_x[34] = [4.9, 3.1, 1.5, 0.2]
+    original_r_data_x[37] = [4.9, 3.6, 1.4, 0.1]
+    iris_x, iris_y = iris_data(version='corrected')
+    assert_array_equal(original_r_data_x, iris_x)
+
+
+def test_iris_invalid_choice():
+    assert_raises(ValueError, iris_data, 'wrong-choice')

203b43b1d16fa073c537038dae76a8581cbe287d
Gabriel Azevedo <gabriel.azevedo@biofourmis.com>
2019-09-25 09:42:43 +0800

    calling iris inside test function instead of outside

--- mlxtend/plotting/tests/test_pca_corr_graph.py
+++ mlxtend/plotting/tests/test_pca_corr_graph.py
@@ -2,14 +2,14 @@ from mlxtend.plotting import plot_pca_correlation_graph
 from mlxtend.data import iris_data
 from sklearn.decomposition.pca import PCA
 
-X, y = iris_data()
-
 
 def test_pass_pca_corr():
+    X, y = iris_data()
     plot_pca_correlation_graph(X, ['1', '2', '3', '4'])
 
 
 def test_pass_pca_corr_pca_out():
+    X, y = iris_data()
     pca = PCA(n_components=2)
     X_pca = pca.fit_transform(X)
     eigen = pca.explained_variance_

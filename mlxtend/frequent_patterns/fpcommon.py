rasbt/mlxtend

commit 22915a8f0ff35fb11ddd574398f7ba53ddbc7a7a

limitations on columns of sparse DaraFrame still apply
Contrary to what I thought in 0a8fa8f, this issue is still not fixed.

mlxtend/frequent_patterns/fpcommon.py
@@ -50,7 +50,7 @@ def generate_itemsets(generator, num_itemsets, colname_map):
 
 
 def valid_input_check(df):
-    if hasattr(df, "to_coo"):
+    if hasattr(df, "to_coo") or hasattr(df, "sparse"):
         if not isinstance(df.columns[0], str) and df.columns[0] != 0:
             raise ValueError('Due to current limitations in Pandas, '
                              'if the SparseDataFrame has integer column names,'

commit 058ec7c8f5ab29b6e2795d641380d772712b086e

fix bug introduced by commit 22915a8 with empty DataFrame

mlxtend/frequent_patterns/fpcommon.py
@@ -50,6 +50,8 @@ def generate_itemsets(generator, num_itemsets, colname_map):
 
 
 def valid_input_check(df):
+    if df.size == 0:
+        return
     if hasattr(df, "to_coo") or hasattr(df, "sparse"):
         if not isinstance(df.columns[0], str) and df.columns[0] != 0:
             raise ValueError('Due to current limitations in Pandas, '
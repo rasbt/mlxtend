rasbt/mlxtend

commit 22915a8f0ff35fb11ddd574398f7ba53ddbc7a7a

limitations on columns of sparse DaraFrame still apply
Contrary to what I thought in 0a8fa8f, this issue is still not fixed.

mlxtend/frequent_patterns/fpcommon.py
@@ -50,7 +50,7 @@ def generate_itemsets(generator, num_itemsets, colname_map):
 
 
 def valid_input_check(df):
-    if hasattr(df, "to_coo"):
+    if hasattr(df, "to_coo") or hasattr(df, "sparse"):
         if not isinstance(df.columns[0], str) and df.columns[0] != 0:
             raise ValueError('Due to current limitations in Pandas, '
                              'if the SparseDataFrame has integer column names,'

commit fa643e20ccca72485135c772996202a7ea76d724

add support for newer pandas sparse dataframes in frequent_patterns (#621)
* add support for newer pandas sparse dataframes in frequent_patterns

SparseDataFrame has been deprecated, pandas now recommands to create
standard DataFrames and store sparse Series as SparseArray.  This
allows to combine both dense and sparse columns.

Improve valid_input_check with sparse values; it previously uncompressed
the whole dataframe to check for invalid values, which defeats its purpose.
It now only checks existing values, which prevents memory error, and is
also much faster.

Update apriori.ipynb notebook to use the new pandas sparse DataFrame.

* use pandas version number to detect new sparse dataframes

* limitations on columns of sparse DaraFrame still apply

Contrary to what I thought in 0a8fa8f, this issue is still not fixed.

* fix bug introduced by commit 22915a8 with empty DataFrame

docs/sources/user_guide/frequent_patterns/apriori.ipynb
@@ -913,7 +913,7 @@
    ],
    "source": [
     "oht_ary = te.fit(dataset).transform(dataset, sparse=True)\n",
-    "sparse_df = pd.SparseDataFrame(oht_ary, columns=te.columns_, default_fill_value=False)\n",
+    "sparse_df = pd.DataFrame.sparse.from_spmatrix(oht_ary, columns=te.columns_)\n",
     "sparse_df"
    ]
   },

mlxtend/frequent_patterns/apriori.py
@@ -230,11 +230,28 @@ def apriori(df, min_support=0.5, use_colnames=False, max_len=None, verbose=0,
 
     fpc.valid_input_check(df)
 
-    is_sparse = hasattr(df, "to_coo")
-    if is_sparse:
-        X = df.to_coo().tocsc()
+    # sparse attribute exists for both deprecated SparseDataFrame and
+    # DataFrame with SparseArray (pandas >= 0.24); to_coo attribute
+    # exists only for the former, thus it is checked first to distinguish
+    # between SparseDataFrame and DataFrame with SparseArray.
+    if hasattr(df, "to_coo"):
+        # SparseDataFrame with pandas < 0.24
+        if df.size == 0:
+            X = df.values
+        else:
+            X = df.to_coo().tocsc()
+        is_sparse = True
+    elif hasattr(df, "sparse"):
+        # DataFrame with SparseArray (pandas >= 0.24)
+        if df.size == 0:
+            X = df.values
+        else:
+            X = df.sparse.to_coo().tocsc()
+        is_sparse = True
     else:
+        # dense DataFrame
         X = df.values
+        is_sparse = False
     support = _support(X, X.shape[0], is_sparse)
     ary_col_idx = np.arange(X.shape[1])
     support_dict = {1: support[support >= min_support]}

mlxtend/frequent_patterns/fpcommon.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import collections
+from distutils.version import LooseVersion as Version
+from pandas import __version__ as pandas_version
 
 
 def setup_fptree(df, min_support):
@@ -48,24 +50,44 @@ def generate_itemsets(generator, num_itemsets, colname_map):
 
 
 def valid_input_check(df):
-    # Fast path: if all columns are boolean, there is nothing to check
-    if not (df.dtypes == bool).all():
-        # Pandas is much slower than numpy, so use df.values instead of df here
-        idxs = np.where((df.values != 1) & (df.values != 0))
-        if len(idxs[0]) > 0:
-            val = df.values[idxs[0][0], idxs[1][0]]
-            s = ('The allowed values for a DataFrame'
-                 ' are True, False, 0, 1. Found value %s' % (val))
-            raise ValueError(s)
-
-    is_sparse = hasattr(df, "to_coo")
-    if is_sparse:
+    if df.size == 0:
+        return
+    if hasattr(df, "to_coo") or hasattr(df, "sparse"):
         if not isinstance(df.columns[0], str) and df.columns[0] != 0:
             raise ValueError('Due to current limitations in Pandas, '
                              'if the SparseDataFrame has integer column names,'
                              'names, please make sure they either start '
                              'with `0` or cast them as string column names: '
                              '`df.columns = [str(i) for i in df.columns`].')
+
+    # Fast path: if all columns are boolean, there is nothing to check
+    if Version(pandas_version) >= Version("0.24"):
+        all_bools = ((df.dtypes == pd.SparseDtype(bool)) |
+                     (df.dtypes == bool)).all()
+    else:
+        all_bools = (df.dtypes == bool).all()
+    if not all_bools:
+        # Pandas is much slower than numpy, so use np.where on Numpy arrays
+        if hasattr(df, "to_coo"):
+            # see comment in apriori.py, to_coo attribute must be checked first
+            if df.size == 0:
+                values = df.values
+            else:
+                values = df.to_coo().tocoo().data
+        elif hasattr(df, "sparse"):
+            if df.size == 0:
+                values = df.values
+            else:
+                values = df.sparse.to_coo().tocoo().data
+        else:
+            values = df.values
+        idxs = np.where((values != 1) & (values != 0))
+        if len(idxs[0]) > 0:
+            # idxs has 1 dimension with sparse data and 2 with dense data
+            val = values[tuple(loc[0] for loc in idxs)]
+            s = ('The allowed values for a DataFrame'
+                 ' are True, False, 0, 1. Found value %s' % (val))
+            raise ValueError(s)
 
 
 class FPTree(object):

mlxtend/frequent_patterns/tests/test_fpbase.py
@@ -9,6 +9,8 @@ from numpy.testing import assert_array_equal
 from mlxtend.utils import assert_raises
 from mlxtend.preprocessing import TransactionEncoder
 import pandas as pd
+from pandas import __version__ as pandas_version
+from distutils.version import LooseVersion as Version
 import sys
 from contextlib import contextmanager
 from io import StringIO
@@ -69,13 +71,19 @@ class FPTestErrors(object):
             assert isinstance(i, frozenset) is True
 
     def test_raise_error_if_input_is_not_binary(self):
+        def test_with_dataframe(df):
+            assert_raises(ValueError,
+                          'The allowed values for a DataFrame are True, '
+                          'False, 0, 1. Found value 2',
+                          self.fpalgo, df)
         df2 = pd.DataFrame(self.one_ary, columns=self.cols).copy()
         df2.iloc[3, 3] = 2
-
-        assert_raises(ValueError,
-                      'The allowed values for a DataFrame are True, '
-                      'False, 0, 1. Found value 2',
-                      self.fpalgo, df2)
+        test_with_dataframe(df2)
+        sdf = df2.to_sparse()
+        test_with_dataframe(sdf)
+        if Version(pandas_version) >= Version("0.24"):
+            sdf2 = df2.astype(pd.SparseDtype(int, fill_value=0))
+            test_with_dataframe(sdf2)
 
     def test_sparsedataframe_notzero_column(self):
         dfs = pd.SparseDataFrame(self.df)
@@ -131,9 +139,31 @@ class FPTestEx1(object):
                       == frozenset(('Kidney Beans', 'Milk'))].values.shape \
             == (1, 2)
 
-    def test_sparse(self):
+    def test_sparse_deprecated(self):
         def test_with_fill_values(fill_value):
             sdf = self.df.to_sparse(fill_value=fill_value)
+            res_df = self.fpalgo(sdf, use_colnames=True)
+            assert res_df.values.shape == self.fpalgo(self.df).values.shape
+            assert res_df[res_df['itemsets']
+                          == 'nothing'].values.shape == (0, 2)
+            assert res_df[res_df['itemsets']
+                          == {'Milk', 'Kidney Beans'}].values.shape == (1, 2)
+            assert res_df[res_df['itemsets'] ==
+                          frozenset(('Milk', 'Kidney Beans'))].values.shape \
+                == (1, 2)
+            assert res_df[res_df['itemsets'] ==
+                          frozenset(('Kidney Beans', 'Milk'))].values.shape \
+                == (1, 2)
+        test_with_fill_values(0)
+        test_with_fill_values(False)
+
+    def test_sparse(self):
+        if Version(pandas_version) < Version("0.24"):
+            return
+
+        def test_with_fill_values(fill_value):
+            sdt = pd.SparseDtype(type(fill_value), fill_value=fill_value)
+            sdf = self.df.astype(sdt)
             res_df = self.fpalgo(sdf, use_colnames=True)
             assert res_df.values.shape == self.fpalgo(self.df).values.shape
             assert res_df[res_df['itemsets']
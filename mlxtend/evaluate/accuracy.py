8be486b491284f50319240cd0dfe3ce108c0635a
Deepan Das <ddas@scortex.io>
2019-11-14 08:44:48 +0100

    Generalization of per_class_accuracy to accuracy
    Function capable of finding different kinds of accuracy metrics in three
    different 'method' modes.

--- mlxtend/evaluate/accuracy.py
+++ mlxtend/evaluate/accuracy.py
@@ -7,20 +7,26 @@
 import numpy as np
 
 
-def per_class_accuracy(y_target, y_predicted, pos_label=None, binary=False):
-    """Compute per_class_accuracy for supervised learning.
+def _compute_metric(target_temp, predicted_temp):
+    return (target_temp == predicted_temp).sum() / float(target_temp.shape[0])
+
+
+def accuracy_score(y_target, y_predicted, method="binary", pos_label=1):
+    """General accuracy function for supervised learning.
     Parameters
     ------------
     y_target : array-like, shape=[n_values]
         True class labels or target values.
     y_predicted : array-like, shape=[n_values]
         Predicted class labels or target values.
-    pos_label : str or int, None by default.
+    method : str, 'standard' by default.
+        The chosen method for accuracy computation.
+        If set to 'standard', computes overall accuracy.
+        If set to 'binary', computes accuracy for class pos_label.
+        If set to 'average', computes average per class accuracy.
+    pos_label : str or int, 1 by default.
         The class whose accuracy score is to be reported.
-        'binary' has to be set to True.
-    binary : bool
-        Projects a multi-label problem to a binary problem.
-        If set to True, it uses `pos_label` to binarize.
+        Used only when method is set to 'binary'
 
     Returns
     ------------
@@ -29,15 +35,23 @@ def per_class_accuracy(y_target, y_predicted, pos_label=None, binary=False):
 
     target_temp = np.asarray(y_target)
     predicted_temp = np.asarray(y_predicted)
+    unique_labels = np.unique(target_temp)
 
     if len(y_target) != len(y_predicted):
         raise AttributeError('`y_target` and `y_predicted`'
                              ' don\'t have the same number of elements.')
-    if binary and pos_label is not None:
-        if pos_label not in np.unique(target_temp):
+    if method == "standard":
+        return _compute_metric(target_temp, predicted_temp)
+
+    elif method == "binary":
+        if pos_label not in unique_labels:
             raise AttributeError("Chosen value of pos_label doesn't exist")
 
         target_temp = np.where(target_temp == pos_label, 1, 0)
         predicted_temp = np.where(predicted_temp == pos_label, 1, 0)
+        return _compute_metric(target_temp, predicted_temp)
 
-    return (target_temp == predicted_temp).sum() / float(target_temp.shape[0])
+    else:
+        return sum([_compute_metric(np.where(target_temp != l, 1, 0),
+                    np.where(predicted_temp != l, 1, 0))
+                    for l in unique_labels]) / float(unique_labels.shape[0])

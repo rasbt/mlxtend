8ddcb72a86a545019dd2ce32c5316eda512c6aa1
Deepan Das <ddas@scortex.io>
2019-11-15 00:47:32 +0100

    adding non-normalized tests

--- mlxtend/evaluate/tests/test_accuracy.py
+++ mlxtend/evaluate/tests/test_accuracy.py
@@ -13,9 +13,12 @@ def test_multiclass_binary():
 
     y_1 = accuracy_score(y_targ, y_pred, method='binary', pos_label=1)
     y_2 = accuracy_score(y_targ, y_pred, method='binary', pos_label=2)
+    y_3 = accuracy_score(y_targ, y_pred, method='binary', pos_label=1,
+                         normalize=False)
 
     assert_almost_equal(y_2, float(7) / 9, decimal=4)
     assert_almost_equal(y_1, float(6) / 9, decimal=4)
+    assert_almost_equal(y_3, 7)
 
 
 def test_standard():
@@ -23,8 +26,10 @@ def test_standard():
     y_pred = [0, 1, 1, 0, 1, 1]
 
     y = accuracy_score(y_targ, y_pred, method='standard')
-
+    y_1 = accuracy_score(y_targ, y_pred, method='binary',
+                         normalize=False)
     assert_almost_equal(y, float(3) / 6, decimal=4)
+    assert_almost_equal(y_1, 3)
 
 
 def test_average():

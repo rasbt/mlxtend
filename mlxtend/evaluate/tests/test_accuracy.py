11234e07561209306bfb8c941870e00483193a1a
Deepan Das <ddas@scortex.io>
2019-11-14 09:09:22 +0100

    Added tests for new accuracy function

--- mlxtend/evaluate/tests/test_accuracy.py
+++ mlxtend/evaluate/tests/test_accuracy.py
@@ -2,26 +2,34 @@
 # mlxtend Machine Learning Library Extensions
 #
 # License: BSD 3 clause
-
-from mlxtend.evaluate import per_class_accuracy
+import numpy as np
+from mlxtend.evaluate import accuracy_score
 from numpy.testing import assert_almost_equal
 
 
-def test_multiclass():
+def test_multiclass_binary():
     y_targ = [0, 0, 0, 1, 1, 1, 2, 2, 2]
     y_pred = [1, 0, 0, 0, 1, 2, 0, 2, 2]
 
-    y_1 = per_class_accuracy(y_targ, y_pred, binary=True, pos_label=1)
-    y_2 = per_class_accuracy(y_targ, y_pred, binary=True, pos_label=2)
+    y_1 = accuracy_score(y_targ, y_pred, method='binary', pos_label=1)
+    y_2 = accuracy_score(y_targ, y_pred, method='binary', pos_label=2)
 
-    assert_almost_equal(y_2, float(7)/9, decimal=4)
-    assert_almost_equal(y_1, float(6)/9, decimal=4)
+    assert_almost_equal(y_2, float(7) / 9, decimal=4)
+    assert_almost_equal(y_1, float(6) / 9, decimal=4)
 
 
-def test_binary():
+def test_standard():
     y_targ = [0, 0, 0, 1, 1, 1]
     y_pred = [0, 1, 1, 0, 1, 1]
 
-    y = per_class_accuracy(y_targ, y_pred)
+    y = accuracy_score(y_targ, y_pred, method='standard')
 
-    assert_almost_equal(y, float(3)/6, decimal=4)
+    assert_almost_equal(y, float(3) / 6, decimal=4)
+
+
+def test_average():
+    y_targ = np.array([0, 0, 0, 1, 1, 1, 1, 1, 2, 2])
+    y_pred = np.array([0, 1, 1, 0, 1, 1, 2, 2, 2, 2])
+
+    y = accuracy_score(y_targ, y_pred, method='average')
+    assert_almost_equal(y, 0.6667)

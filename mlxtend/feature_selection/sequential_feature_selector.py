0b079f75bd816d516d5ea52b30258ce4a34a307f
Lareinaxy <lareinaliu1997@gmail.com>
2019-12-21 16:28:14 -0600

    formated

--- mlxtend/feature_selection/sequential_feature_selector.py
+++ mlxtend/feature_selection/sequential_feature_selector.py
@@ -382,8 +382,8 @@ class SequentialFeatureSelector(_BaseXComposition, MetaEstimatorMixin):
 
         else:
             select_in_range = False
-            k_to_select = int(len(X[1])**.5) # random subsample of sqrt(k_features) dimensions
-            np.take(X, np.random.permutation(X.shape[1]), axis = 1, out = X) # shuffle matrix horizontally
+            k_to_select = int(len(X[1])**.5)
+            np.take(X, np.random.permutation(X.shape[1]), axis=1, out=X)
 
         orig_set = set(range(X_.shape[1]))
         n_features = X_.shape[1]

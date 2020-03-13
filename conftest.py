5acd235e5910ae76f965eee87cb2ccb19ee6fee5
rasbt <mail@sebastianraschka.com>
2020-03-12 22:41:41 -0500

    mod conftest

--- conftest.py
+++ conftest.py
@@ -8,5 +8,5 @@ sys.path.insert(0, HERE)
 
 collect_ignore = []
 if 'TRAVIS' in os.environ or os.environ.get('TRAVIS') == 'true':
-    collect_ignore.append("tests/test_extract_face_landmarks.py")
-    collect_ignore.append("tests/test_eyepad_align.py")
+    collect_ignore.append("mlxtend/tests/test_extract_face_landmarks.py")
+    collect_ignore.append("mlxtend/tests/test_eyepad_align.py")

f560a58ce866bca00001f120a1eb1ba730b5389c
rasbt <mail@sebastianraschka.com>
2020-03-12 22:29:53 -0500

    mod conftest

--- mlxtend/image/conftest.py
+++ mlxtend/image/conftest.py
@@ -1,6 +1,10 @@
 # content of conftest.py
 import os
 
+HERE = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, HERE)
+
+
 collect_ignore = []
 if 'TRAVIS' in os.environ or os.environ.get('TRAVIS') == 'true':
     collect_ignore.append("tests/test_extract_face_landmarks.py")

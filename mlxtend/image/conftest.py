de499e256bbdf0a0c6784c111a6bcccf5feee8ec
rasbt <mail@sebastianraschka.com>
2020-03-12 22:22:46 -0500

    mod conftest

--- mlxtend/image/conftest.py
+++ mlxtend/image/conftest.py
@@ -2,6 +2,6 @@
 import os
 
 collect_ignore = []
-if 'TRAVIS' in os.environ:
+if 'TRAVIS' in os.environ or os.environ.get('TRAVIS') == 'true':
     collect_ignore.append("tests/test_extract_face_landmarks.py")
     collect_ignore.append("tests/test_eyepad_align.py")

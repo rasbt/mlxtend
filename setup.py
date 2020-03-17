41fbdf7513a8e5830433d5e13d8d94a77ef10347
harjeet <harjeet.kalsi@solirius.com>
2020-03-17 09:10:59 +0000

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mlxtend',
-      version='0.0.2',
+      version='0.0.3',
       description='Machine Learning Library Extensions',
       url='https://github.com/pelucid/mlxtend',
       packages=find_packages(),

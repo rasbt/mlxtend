ee26e25c57b57fbb9373a034692404b9bd21da31
rasbt <mail@sebastianraschka.com>
2020-03-22 21:02:04 -0500

    fix pep8 issues

--- conftest.py
+++ conftest.py
@@ -1,12 +1,11 @@
 # content of conftest.py
- import os
- import sys
+import os
+import sys
 
-  HERE = os.path.dirname(os.path.abspath(__file__))
- sys.path.insert(0, HERE)
+HERE = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, HERE)
 
- 
-  collect_ignore = []
- if 'TRAVIS' in os.environ or os.environ.get('TRAVIS') == 'true':
-     collect_ignore.append("mlxtend/tests/test_extract_face_landmarks.py")
-     collect_ignore.append("mlxtend/tests/test_eyepad_align.py")
+collect_ignore = []
+if 'TRAVIS' in os.environ or os.environ.get('TRAVIS') == 'true':
+    collect_ignore.append("mlxtend/tests/test_extract_face_landmarks.py")
+    collect_ignore.append("mlxtend/tests/test_eyepad_align.py")

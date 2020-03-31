e55291109058f57166768361236774823605eaa3
rasbt <mail@sebastianraschka.com>
2020-03-31 18:21:49 -0500

    add ignore_subpackages option to make_api.py

---
+++ .travis_test.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+python --version
+python -c "import pandas; print('pandas %s' % pandas.__version__)"
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "import sklearn; print('sklearn %s' % sklearn.__version__)"
+python -c "import mlxtend; print('mlxtend %s' % mlxtend.__version__)"
+
+if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then 
+
+
+        if [[ "$COVERAGE" == "true" ]]; then
+
+            if [[ "$IMAGE" == "true" ]]; then
+                 PYTHONPATH='.' pytest -sv --cov=mlxtend
+            else
+                 PYTHONPATH='.' pytest -sv --cov=mlxtend --ignore=mlxtend/image
+            fi
+
+        else
+            if [[ "$IMAGE" == "true" ]]; then
+                 PYTHONPATH='.' pytest -sv
+            else
+                 PYTHONPATH='.' pytest -sv --ignore=mlxtend/image
+            fi
+        fi
+
+else
+     PYTHONPATH='.' pytest -sv --ignore=mlxtend/plotting
+fi
+  
+
+if [[ "$NOTEBOOKS" == "true" ]]; then
+    cd docs
+    python make_api.py
+    # skip image subdir because DLIB takes too long to download on travis
+
+    if [[ "$IMAGE" == "false" ]]; then
+      python make_api.py
+      find sources -name "*.ipynb" -not -path "sources/user_guide/image/*" -exec jupyter nbconvert --to notebook --execute {} \;
+    else
+      python make_api.py --ignore_subpackages "mlxtend.image"
+      find sources -name "*.ipynb" -exec jupyter nbconvert --to notebook --execute {} \;
+    fi
+fi

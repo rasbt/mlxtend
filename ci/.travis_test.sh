ed9e8869a696b997870e82c94c6d53c9e123fc6a
rasbt <mail@sebastianraschka.com>
2020-03-31 18:30:51 -0500

    add ignore_subpackages option to make_api.py

--- ci/.travis_test.sh
+++ ci/.travis_test.sh
@@ -39,10 +39,10 @@ if [[ "$NOTEBOOKS" == "true" ]]; then
     # skip image subdir because DLIB takes too long to download on travis
 
     if [[ "$IMAGE" == "false" ]]; then
-      python make_api.py
+      python make_api.py --ignore_subpackages "mlxtend.image"
       find sources -name "*.ipynb" -not -path "sources/user_guide/image/*" -exec jupyter nbconvert --to notebook --execute {} \;
     else
-      python make_api.py --ignore_subpackages "mlxtend.image"
+      python make_api.py 
       find sources -name "*.ipynb" -exec jupyter nbconvert --to notebook --execute {} \;
     fi
 fi

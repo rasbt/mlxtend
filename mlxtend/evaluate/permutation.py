fix pvalue calculation

mlxtend/evaluate/permutation.py
@@ -97,7 +97,7 @@ def permutation_test(x, y, func='x_mean != y_mean', method='exact',
     m, n = len(x), len(y)
     combined = np.hstack((x, y))
 
-    more_extreme = 0.
+    at_least_as_extreme = 0.
     reference_stat = func(x, y)
 
     # Note that whether we compute the combinations or permutations
@@ -120,15 +120,21 @@ def permutation_test(x, y, func='x_mean != y_mean', method='exact',
             indices_y = [i for i in range(m + n) if i not in indices_x]
             diff = func(combined[list(indices_x)], combined[indices_y])
 
-            if diff > reference_stat:
-                more_extreme += 1.
+            if diff > reference_stat or np.isclose(diff, reference_stat):
+                at_least_as_extreme += 1.
 
         num_rounds = factorial(m + n) / (factorial(m)*factorial(n))
 
     else:
         for i in range(num_rounds):
             rng.shuffle(combined)
-            if func(combined[:m], combined[m:]) > reference_stat:
-                more_extreme += 1.
+            diff = func(combined[:m], combined[m:])
 
-    return more_extreme / num_rounds
+            if diff > reference_stat or np.isclose(diff, reference_stat):
+                at_least_as_extreme += 1.
+
+        # To cover the actual experiment results
+        at_least_as_extreme += 1
+        num_rounds += 1
+
+    return at_least_as_extreme / num_rounds

mlxtend/evaluate/tests/test_permutation.py
@@ -16,36 +16,36 @@ control = [657, 623, 652, 654, 658, 660, 670, 620]
 def test_one_sided_x_greater_y():
     p = permutation_test(treatment, control,
                          func=lambda x, y: np.mean(x) - np.mean(y))
-    assert round(p, 4) == 0.0274, p
+    assert round(p, 4) == 0.0301, p
 
     p = permutation_test(treatment, control,
                          func="x_mean > y_mean")
-    assert round(p, 4) == 0.0274, p
+    assert round(p, 4) == 0.0301, p
 
 
 def test_one_sided_y_greater_x():
     p = permutation_test(treatment, control,
                          func=lambda x, y: np.mean(y) - np.mean(x))
-    assert round(p, 3) == 1 - 0.03, p
+    assert round(p, 3) == 0.973, p
 
     p = permutation_test(treatment, control,
                          func="x_mean < y_mean")
-    assert round(p, 3) == 1 - 0.03, p
+    assert round(p, 3) == 0.973, p
 
 
 def test_two_sided():
     p = permutation_test(treatment, control,
                          func=lambda x, y: np.abs(np.mean(x) - np.mean(y)))
-    assert round(p, 3) == 0.055, p
+    assert round(p, 3) == 0.060, p
 
     p = permutation_test(treatment, control,
                          func="x_mean != y_mean")
-    assert round(p, 3) == 0.055, p
+    assert round(p, 3) == 0.060, p
 
 
 def test_default():
     p = permutation_test(treatment, control)
-    assert round(p, 3) == 0.055, p
+    assert round(p, 3) == 0.060, p
 
 
 def test_approximateone_sided_x_greater_y():
@@ -55,7 +55,7 @@ def test_approximateone_sided_x_greater_y():
                          method='approximate',
                          num_rounds=5000,
                          seed=123)
-    assert round(p, 3) == 0.028, p
+    assert round(p, 3) == 0.031, p
 
 
 def test_invalid_method():
884c162263c636c805823796656ec3de9cfc9efc
geo <apremgeorge@gmail.com>
2020-09-04 12:09:22 +1000

    Keras Sequential Model Support for Bias Variance Computation

--- mlxtend/evaluate/bias_variance_decomp.py
+++ mlxtend/evaluate/bias_variance_decomp.py
@@ -74,8 +74,13 @@ def bias_variance_decomp(estimator, X_train, y_train, X_test, y_test,
 
     for i in range(num_rounds):
         X_boot, y_boot = _draw_bootstrap_sample(rng, X_train, y_train)
-        pred = estimator.fit(X_boot, y_boot).predict(X_test)
-        all_pred[i] = pred
+        if estimator.__class__.__name__ == 'Sequential':
+            estimator.fit(X_boot, y_boot)
+            pred = estimator.predict(X_test)
+            all_pred[i] = pred.reshape(1,-1)
+        else:    
+            pred = estimator.fit(X_boot, y_boot).predict(X_test)
+            all_pred[i] = pred
 
     if loss == '0-1_loss':
         main_predictions = np.apply_along_axis(lambda x:

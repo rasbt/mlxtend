46947e375ab614a800fe60e179f8581cf2734f00
Harjeet Kalsi <33220635+harjeetkalsi@users.noreply.github.com>
2021-03-22 13:27:22 +0000

    Pin version to stop conflicts

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='mlxtend',
-      version='0.0.1',
+      version='0.0.2',
       description='Machine Learning Library Extensions',
       url='https://github.com/pelucid/mlxtend',
       packages=find_packages(),
       install_requires=[
-          "scipy>=1.2.1",
+          "scipy~=1.1.0",
           "numpy>=1.16.2",
           "pandas>=0.24.2",
           "scikit-learn>=0.20.3",

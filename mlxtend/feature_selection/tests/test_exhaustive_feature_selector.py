7c070be5bf0700f38031577f8cd4a8659e3cf191
rasbt <mail@sebastianraschka.com>
2021-11-01 11:34:24 -0500

    scikit-learn 1.0 patch

--- mlxtend/feature_selection/tests/test_exhaustive_feature_selector.py
+++ mlxtend/feature_selection/tests/test_exhaustive_feature_selector.py
@@ -4,7 +4,6 @@
 #
 # License: BSD 3 clause
 
-import sys
 import numpy as np
 import pandas as pd
 from distutils.version import LooseVersion as Version
@@ -160,22 +159,27 @@ def test_knn_cv3():
                cv=4,
                print_progress=False)
     efs1 = efs1.fit(X, y)
-    expect = {0: {'avg_score': 0.9391025641025641,
+    expect = {0: {'avg_score': 0.9329658605974395,
                   'feature_idx': (0, 1, 2),
                   'feature_names': ('0', '1', '2'),
-                  'cv_scores': np.array([0.974, 0.947, 0.892, 0.946])},
+                  'cv_scores': np.array([0.974, 0.947, 0.892, 0.919])},
               1: {'avg_score': 0.9400782361308677,
                   'feature_idx': (0, 1, 3),
                   'feature_names': ('0', '1', '3'),
                   'cv_scores': np.array([0.921, 0.947, 0.919, 0.973])},
-              2: {'avg_score': 0.95299145299145294,
+              2: {'avg_score': 0.9532361308677098,
                   'feature_idx': (0, 2, 3),
                   'feature_names': ('0', '2', '3'),
                   'cv_scores': np.array([0.974, 0.947, 0.919, 0.973])},
               3: {'avg_score': 0.97275641025641035,
                   'feature_idx': (1, 2, 3),
                   'feature_names': ('1', '2', '3'),
-                  'cv_scores': np.array([0.974, 1.   , 0.946, 0.973])}}
+                  'cv_scores': np.array([0.974, 1., 0.946, 0.973])}}
+
+    if Version(sklearn_version) < Version("1.0"):
+        expect[0]['avg_score'] = 0.9391025641025641
+        expect[0]['cv_scores'] = np.array([0.974, 0.947, 0.892, 0.946])
+        expect[2]['avg_score'] = 0.9529914529914529
 
     if Version(sklearn_version) < Version("0.22"):
         expect[0]['cv_scores'] = np.array([0.97435897, 0.94871795,

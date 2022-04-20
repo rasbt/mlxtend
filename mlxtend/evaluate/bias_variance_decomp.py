dcc601b98ef69c3fdbb830ac1c996fcb3b297c59
rasbt <mail@sebastianraschka.com>
2022-04-20 12:06:32 -0500

    upd min ex

--- mlxtend/evaluate/bias_variance_decomp.py
+++ mlxtend/evaluate/bias_variance_decomp.py
@@ -10,15 +10,23 @@ import numpy as np
 
 def _draw_bootstrap_sample(rng, X, y):
     sample_indices = np.arange(X.shape[0])
-    bootstrap_indices = rng.choice(sample_indices,
-                                   size=sample_indices.shape[0],
-                                   replace=True)
+    bootstrap_indices = rng.choice(
+        sample_indices, size=sample_indices.shape[0], replace=True
+    )
     return X[bootstrap_indices], y[bootstrap_indices]
 
 
-def bias_variance_decomp(estimator, X_train, y_train, X_test, y_test,
-                         loss='0-1_loss', num_rounds=200, random_seed=None,
-                         **fit_params):
+def bias_variance_decomp(
+    estimator,
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    loss="0-1_loss",
+    num_rounds=200,
+    random_seed=None,
+    **fit_params
+):
     """
     estimator : object
         A classifier or regressor object or class implementing both a
@@ -69,26 +77,27 @@ def bias_variance_decomp(estimator, X_train, y_train, X_test, y_test,
     http://rasbt.github.io/mlxtend/user_guide/evaluate/bias_variance_decomp/
 
     """
-    supported = ['0-1_loss', 'mse']
+    supported = ["0-1_loss", "mse"]
     if loss not in supported:
-        raise NotImplementedError('loss must be one of the following: %s' %
-                                  supported)
+        raise NotImplementedError("loss must be one of the following: %s" % supported)
 
     for ary in (X_train, y_train, X_test, y_test):
-        if hasattr(ary, 'loc'):
-            raise ValueError('The bias_variance_decomp does not '
-                             'support pandas DataFrames yet. '
-                             'Please check the inputs to '
-                             'X_train, y_train, X_test, y_test. '
-                             'If e.g., X_train is a pandas '
-                             'DataFrame, try passing it as NumPy array via '
-                             'X_train=X_train.values.')
+        if hasattr(ary, "loc"):
+            raise ValueError(
+                "The bias_variance_decomp does not "
+                "support pandas DataFrames yet. "
+                "Please check the inputs to "
+                "X_train, y_train, X_test, y_test. "
+                "If e.g., X_train is a pandas "
+                "DataFrame, try passing it as NumPy array via "
+                "X_train=X_train.values."
+            )
 
     rng = np.random.RandomState(random_seed)
 
-    if loss == '0-1_loss':
+    if loss == "0-1_loss":
         dtype = np.int64
-    elif loss == 'mse':
+    elif loss == "mse":
         dtype = np.float64
 
     all_pred = np.zeros((num_rounds, y_test.shape[0]), dtype=dtype)
@@ -97,61 +106,58 @@ def bias_variance_decomp(estimator, X_train, y_train, X_test, y_test,
         X_boot, y_boot = _draw_bootstrap_sample(rng, X_train, y_train)
 
         # Keras support
-        if estimator.__class__.__name__ in ['Sequential', 'Functional']:
+        if estimator.__class__.__name__ in ["Sequential", "Functional"]:
 
             # reset model
             for ix, layer in enumerate(estimator.layers):
-                if hasattr(estimator.layers[ix], 'kernel_initializer') and \
-                        hasattr(estimator.layers[ix], 'bias_initializer'):
-                    weight_initializer = \
-                        estimator.layers[ix].kernel_initializer
+                if hasattr(estimator.layers[ix], "kernel_initializer") and hasattr(
+                    estimator.layers[ix], "bias_initializer"
+                ):
+                    weight_initializer = estimator.layers[ix].kernel_initializer
                     bias_initializer = estimator.layers[ix].bias_initializer
 
-                    old_weights, old_biases = \
-                        estimator.layers[ix].get_weights()
+                    old_weights, old_biases = estimator.layers[ix].get_weights()
 
-                    estimator.layers[ix].set_weights([
-                        weight_initializer(shape=old_weights.shape),
-                        bias_initializer(shape=len(old_biases))])
+                    estimator.layers[ix].set_weights(
+                        [
+                            weight_initializer(shape=old_weights.shape),
+                            bias_initializer(shape=len(old_biases)),
+                        ]
+                    )
 
             estimator.fit(X_boot, y_boot, **fit_params)
             pred = estimator.predict(X_test).reshape(1, -1)
         else:
-            pred = estimator.fit(
-                X_boot, y_boot, **fit_params).predict(X_test)
+            pred = estimator.fit(X_boot, y_boot, **fit_params).predict(X_test)
         all_pred[i] = pred
 
-    if loss == '0-1_loss':
-        main_predictions = np.apply_along_axis(lambda x:
-                                               np.argmax(np.bincount(x)),
-                                               axis=0,
-                                               arr=all_pred)
+    if loss == "0-1_loss":
+        main_predictions = np.apply_along_axis(
+            lambda x: np.argmax(np.bincount(x)), axis=0, arr=all_pred
+        )
 
-        avg_expected_loss = np.apply_along_axis(lambda x:
-                                                (x != y_test).mean(),
-                                                axis=1,
-                                                arr=all_pred).mean()
+        avg_expected_loss = np.apply_along_axis(
+            lambda x: (x != y_test).mean(), axis=1, arr=all_pred
+        ).mean()
 
         avg_bias = np.sum(main_predictions != y_test) / y_test.size
 
         var = np.zeros(pred.shape)
 
         for pred in all_pred:
-            var += (pred != main_predictions).astype(np.int)
+            var += (pred != main_predictions).astype(np.int64)
         var /= num_rounds
 
-        avg_var = var.sum()/y_test.shape[0]
+        avg_var = var.sum() / y_test.shape[0]
 
     else:
         avg_expected_loss = np.apply_along_axis(
-            lambda x:
-            ((x - y_test)**2).mean(),
-            axis=1,
-            arr=all_pred).mean()
+            lambda x: ((x - y_test) ** 2).mean(), axis=1, arr=all_pred
+        ).mean()
 
         main_predictions = np.mean(all_pred, axis=0)
 
-        avg_bias = np.sum((main_predictions - y_test)**2) / y_test.size
-        avg_var = np.sum((main_predictions - all_pred)**2) / all_pred.size
+        avg_bias = np.sum((main_predictions - y_test) ** 2) / y_test.size
+        avg_var = np.sum((main_predictions - all_pred) ** 2) / all_pred.size
 
     return avg_expected_loss, avg_bias, avg_var
--- mlxtend/evaluate/tests/test_bias_variance_decomp.py
+++ mlxtend/evaluate/tests/test_bias_variance_decomp.py
@@ -7,27 +7,23 @@
 # License: BSD 3 clause
 
 
-import os
 import pandas as pd
-import pytest
-from mlxtend.evaluate import bias_variance_decomp
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import BaggingRegressor
 from mlxtend.data import iris_data
 from mlxtend.data import boston_housing_data
+from mlxtend.evaluate import bias_variance_decomp
 from sklearn.model_selection import train_test_split
 
 
 def pandas_input_fail():
 
     X, y = iris_data()
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.3,
-                                                        random_state=123,
-                                                        shuffle=True,
-                                                        stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=123, shuffle=True, stratify=y
+    )
 
     X_train = pd.DataFrame(X_train)
 
@@ -35,25 +31,21 @@ def pandas_input_fail():
 
     with pytest.raises(ValueError):
         avg_expected_loss, avg_bias, avg_var = bias_variance_decomp(
-                tree, X_train, y_train, X_test, y_test,
-                loss='0-1_loss',
-                random_seed=123)
+            tree, X_train, y_train, X_test, y_test, loss="0-1_loss", random_seed=123
+        )
 
 
 def test_01_loss_tree():
 
     X, y = iris_data()
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.3,
-                                                        random_state=123,
-                                                        shuffle=True,
-                                                        stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=123, shuffle=True, stratify=y
+    )
 
     tree = DecisionTreeClassifier(random_state=123)
     avg_expected_loss, avg_bias, avg_var = bias_variance_decomp(
-            tree, X_train, y_train, X_test, y_test,
-            loss='0-1_loss',
-            random_seed=123)
+        tree, X_train, y_train, X_test, y_test, loss="0-1_loss", random_seed=123
+    )
 
     assert round(avg_expected_loss, 3) == 0.062
     assert round(avg_bias, 3) == 0.022
@@ -63,19 +55,15 @@ def test_01_loss_tree():
 def test_01_loss_bagging():
 
     X, y = iris_data()
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.3,
-                                                        random_state=123,
-                                                        shuffle=True,
-                                                        stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=123, shuffle=True, stratify=y
+    )
 
     tree = DecisionTreeClassifier(random_state=123)
-    bag = BaggingClassifier(base_estimator=tree,
-                            random_state=123)
+    bag = BaggingClassifier(base_estimator=tree, random_state=123)
     avg_expected_loss, avg_bias, avg_var = bias_variance_decomp(
-            bag, X_train, y_train, X_test, y_test,
-            loss='0-1_loss',
-            random_seed=123)
+        bag, X_train, y_train, X_test, y_test, loss="0-1_loss", random_seed=123
+    )
 
     assert round(avg_expected_loss, 3) == 0.048
     assert round(avg_bias, 3) == 0.022
@@ -85,16 +73,14 @@ def test_01_loss_bagging():
 def test_mse_tree():
 
     X, y = boston_housing_data()
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.3,
-                                                        random_state=123,
-                                                        shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=123, shuffle=True
+    )
 
     tree = DecisionTreeRegressor(random_state=123)
     avg_expected_loss, avg_bias, avg_var = bias_variance_decomp(
-            tree, X_train, y_train, X_test, y_test,
-            loss='mse',
-            random_seed=123)
+        tree, X_train, y_train, X_test, y_test, loss="mse", random_seed=123
+    )
 
     assert round(avg_expected_loss, 3) == 31.536
     assert round(avg_bias, 3) == 14.096
@@ -104,54 +90,17 @@ def test_mse_tree():
 def test_mse_bagging():
 
     X, y = boston_housing_data()
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.3,
-                                                        random_state=123,
-                                                        shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=123, shuffle=True
+    )
 
     tree = DecisionTreeRegressor(random_state=123)
-    bag = BaggingRegressor(base_estimator=tree,
-                           n_estimators=10,
-                           random_state=123)
+    bag = BaggingRegressor(base_estimator=tree, n_estimators=10, random_state=123)
 
     avg_expected_loss, avg_bias, avg_var = bias_variance_decomp(
-            bag, X_train, y_train, X_test, y_test,
-            loss='mse',
-            random_seed=123)
+        bag, X_train, y_train, X_test, y_test, loss="mse", random_seed=123
+    )
 
     assert round(avg_expected_loss, 2) == 20.24, avg_expected_loss
     assert round(avg_bias, 2) == 15.63, avg_bias
     assert round(avg_var, 2) == 4.61, avg_var
-
-
-if 'TRAVIS' in os.environ or os.environ.get('TRAVIS') == 'true':
-    TRAVIS = True
-else:
-    TRAVIS = False
-
-if 'APPVEYOR' in os.environ or os.environ.get('APPVEYOR') == 'true':
-    APPVEYOR = True
-else:
-    APPVEYOR = False
-
-
-@pytest.mark.skipif(TRAVIS or APPVEYOR, reason="TensorFlow dependency")
-def test_keras():
-
-    import tensorflow as tf
-    X, y = boston_housing_data()
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y,
-        test_size=0.3,
-        random_state=123,
-        shuffle=True)
-
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(32, activation=tf.nn.relu),
-        tf.keras.layers.Dense(1)])
-
-    optimizer = tf.keras.optimizers.Adam()
-    model.compile(loss='mean_squared_error', optimizer=optimizer)
-
-    model.fit(X_train, y_train, epochs=10)
-    model.predict(X_test)

a5ad17a4d50e56df0eb079108b6d84d98f53c38b
rasbt <mail@sebastianraschka.com>
2022-04-23 08:14:46 -0500

    fix doctools

--- docs/make_api.py
+++ docs/make_api.py
@@ -16,7 +16,7 @@ import shutil
 
 
 def _obj_name(obj):
-    if hasattr(obj, '__name__'):
+    if hasattr(obj, "__name__"):
         return obj.__name__
 
 
@@ -26,13 +26,13 @@ def make_markdown_url(line_string, s):
     a markdown link
     """
     new_line = []
-    old_line = line_string.split(' ')
+    old_line = line_string.split(" ")
     for token in old_line:
         if not token.startswith(s):
             new_line.append(token)
         else:
-            new_line.append('[%s](%s)' % (token, token))
-    return ' '.join(new_line)
+            new_line.append("[%s](%s)" % (token, token))
+    return " ".join(new_line)
 
 
 def docstring_to_markdown(docstring):
@@ -52,40 +52,41 @@ def docstring_to_markdown(docstring):
     new_docstring_lst = []
 
     encountered_examples = False
-    for idx, line in enumerate(docstring.split('\n')):
+    for idx, line in enumerate(docstring.split("\n")):
         line = line.strip()
-        if set(line) in ({'-'}, {'='}):
-            new_docstring_lst[idx-1] = '**%s**' % new_docstring_lst[idx-1]
-        elif line.startswith('>>>'):
+        if set(line) in ({"-"}, {"="}):
+            new_docstring_lst[idx - 1] = "**%s**" % new_docstring_lst[idx - 1]
+        elif line.startswith(">>>"):
             if not encountered_examples:
-                new_docstring_lst.append('```')
+                new_docstring_lst.append("```")
                 encountered_examples = True
         new_docstring_lst.append(line)
 
     for idx, line in enumerate(new_docstring_lst[1:]):
         if line:
-            if line.startswith('Description : '):
-                new_docstring_lst[idx+1] = (new_docstring_lst[idx+1]
-                                            .replace('Description : ', ''))
-            elif ' : ' in line:
-                line = line.replace(' : ', '` : ')
-                new_docstring_lst[idx+1] = '\n- `%s\n' % line
-            elif '**' in new_docstring_lst[idx-1] and '**' not in line:
-                new_docstring_lst[idx+1] = '\n%s' % line.lstrip()
-            elif '**' not in line:
-                new_docstring_lst[idx+1] = '    %s' % line.lstrip()
+            if line.startswith("Description : "):
+                new_docstring_lst[idx + 1] = new_docstring_lst[idx + 1].replace(
+                    "Description : ", ""
+                )
+            elif " : " in line:
+                line = line.replace(" : ", "` : ")
+                new_docstring_lst[idx + 1] = "\n- `%s\n" % line
+            elif "**" in new_docstring_lst[idx - 1] and "**" not in line:
+                new_docstring_lst[idx + 1] = "\n%s" % line.lstrip()
+            elif "**" not in line:
+                new_docstring_lst[idx + 1] = "    %s" % line.lstrip()
 
     clean_lst = []
     for line in new_docstring_lst:
-        if set(line.strip()) not in ({'-'}, {'='}):
+        if set(line.strip()) not in ({"-"}, {"="}):
             clean_lst.append(line)
 
     if encountered_examples:
-        clean_lst.append('```')
+        clean_lst.append("```")
     return clean_lst
 
 
-def object_to_markdownpage(obj_name, obj, s=''):
+def object_to_markdownpage(obj_name, obj, s=""):
     """Generate the markdown documentation of a Python object.
 
     Parameters
@@ -104,39 +105,39 @@ def object_to_markdownpage(obj_name, obj, s=''):
 
     """
     # header
-    s += '## %s\n' % obj_name
+    s += "## %s\n" % obj_name
 
     # function/class/method signature
-    sig = str(inspect.signature(obj)).replace('(self, ', '(')
-    s += '\n*%s%s*\n\n' % (obj_name, sig)
+    sig = str(inspect.signature(obj)).replace("(self, ", "(")
+    s += "\n*%s%s*\n\n" % (obj_name, sig)
 
     # docstring body
     doc = str(inspect.getdoc(obj))
     ds = docstring_to_markdown(doc)
-    s += '\n'.join(ds)
+    s += "\n".join(ds)
 
     # document methods
     if inspect.isclass(obj):
-        methods, properties = '\n\n### Methods', '\n\n### Properties'
+        methods, properties = "\n\n### Methods", "\n\n### Properties"
         members = inspect.getmembers(obj)
         for m in members:
-            if not m[0].startswith('_') and len(m) >= 2:
+            if not m[0].startswith("_") and len(m) >= 2:
                 if isinstance(m[1], property):
-                    properties += '\n\n<hr>\n\n*%s*\n\n' % m[0]
+                    properties += "\n\n<hr>\n\n*%s*\n\n" % m[0]
                     m_doc = docstring_to_markdown(str(inspect.getdoc(m[1])))
-                    properties += '\n'.join(m_doc)
+                    properties += "\n".join(m_doc)
                 else:
                     sig = str(inspect.signature(m[1]))
-                    sig = sig.replace('(self, ', '(').replace('(self)', '()')
-                    sig = sig.replace('(self)', '()')
-                    methods += '\n\n<hr>\n\n*%s%s*\n\n' % (m[0], sig)
+                    sig = sig.replace("(self, ", "(").replace("(self)", "()")
+                    sig = sig.replace("(self)", "()")
+                    methods += "\n\n<hr>\n\n*%s%s*\n\n" % (m[0], sig)
                     m_doc = docstring_to_markdown(str(inspect.getdoc(m[1])))
-                    methods += '\n'.join(m_doc)
-        if len(methods) > len('\n\n### Methods'):
+                    methods += "\n".join(m_doc)
+        if len(methods) > len("\n\n### Methods"):
             s += methods
-        if len(properties) > len('\n\n### Properties'):
+        if len(properties) > len("\n\n### Properties"):
             s += properties
-    return s + '\n\n'
+    return s + "\n\n"
 
 
 def import_package(rel_path_to_package, package_name):
@@ -211,7 +212,7 @@ def get_functions_and_classes(package):
     """
     classes, functions = [], []
     for name, member in inspect.getmembers(package):
-        if not name.startswith('_'):
+        if not name.startswith("_"):
             if inspect.isclass(member):
                 classes.append([name, member])
             elif inspect.isfunction(member):
@@ -219,8 +220,9 @@ def get_functions_and_classes(package):
     return classes, functions
 
 
-def generate_api_docs(package, api_dir, clean=False,
-                      printlog=True, ignore_packages=None):
+def generate_api_docs(
+    package, api_dir, clean=False, printlog=True, ignore_packages=None
+):
     """Generate a module level API documentation of a python package.
 
     Description
@@ -246,7 +248,7 @@ def generate_api_docs(package, api_dir, clean=False,
 
     """
     if printlog:
-        print('\n\nGenerating Module Files\n%s\n' % (50 * '='))
+        print("\n\nGenerating Module Files\n%s\n" % (50 * "="))
 
     prefix = package.__name__ + "."
 
@@ -257,12 +259,11 @@ def generate_api_docs(package, api_dir, clean=False,
 
     # get subpackages
     api_docs = {}
-    for importer, pkg_name, is_pkg in pkgutil.iter_modules(
-            package.__path__, prefix):
+    for importer, pkg_name, is_pkg in pkgutil.iter_modules(package.__path__, prefix):
 
         if ignore_packages is not None and pkg_name in ignore_packages:
             if printlog:
-                print('ignored %s' % pkg_name)
+                print("ignored %s" % pkg_name)
             continue
         if is_pkg:
             subpackage = __import__(pkg_name, fromlist="dummy")
@@ -277,45 +278,44 @@ def generate_api_docs(package, api_dir, clean=False,
             if not os.path.isdir(target_dir):
                 os.makedirs(target_dir)
                 if printlog:
-                    print('created %s' % target_dir)
+                    print("created %s" % target_dir)
 
             # create markdown documents in memory
             for obj in classes + functions:
-                md_path = os.path.join(target_dir, obj[0]) + '.md'
+                md_path = os.path.join(target_dir, obj[0]) + ".md"
                 if md_path not in api_docs:
-                    api_docs[md_path] = object_to_markdownpage(obj_name=obj[0],
-                                                               obj=obj[1],
-                                                               s='')
+                    api_docs[md_path] = object_to_markdownpage(
+                        obj_name=obj[0], obj=obj[1], s=""
+                    )
                 else:
-                    api_docs[md_path] += object_to_markdownpage(obj_name=(
-                                                                obj[0]),
-                                                                obj=obj[1],
-                                                                s='')
+                    api_docs[md_path] += object_to_markdownpage(
+                        obj_name=(obj[0]), obj=obj[1], s=""
+                    )
 
     # write to files
     for d in sorted(api_docs):
-        prev = ''
+        prev = ""
         if os.path.isfile(d):
-            with open(d, 'r') as f:
+            with open(d, "r") as f:
                 prev = f.read()
             if prev == api_docs[d]:
-                msg = 'skipped'
+                msg = "skipped"
             else:
-                msg = 'updated'
+                msg = "updated"
         else:
-            msg = 'created'
+            msg = "created"
 
-        if msg != 'skipped':
-            with open(d, 'w') as f:
+        if msg != "skipped":
+            with open(d, "w") as f:
                 f.write(api_docs[d])
 
         if printlog:
-            print('%s %s' % (msg, d))
+            print("%s %s" % (msg, d))
 
 
-def summarize_methdods_and_functions(api_modules, out_dir,
-                                     printlog=False, clean=True,
-                                     str_above_header=''):
+def summarize_methdods_and_functions(
+    api_modules, out_dir, printlog=False, clean=True, str_above_header=""
+):
     """Generates subpacke-level summary files.
 
     Description
@@ -341,7 +341,7 @@ def summarize_methdods_and_functions(api_modules, out_dir,
 
     """
     if printlog:
-        print('\n\nGenerating Subpackage Files\n%s\n' % (50 * '='))
+        print("\n\nGenerating Subpackage Files\n%s\n" % (50 * "="))
 
     if clean:
         if os.path.isdir(out_dir):
@@ -350,102 +350,116 @@ def summarize_methdods_and_functions(api_modules, out_dir,
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
         if printlog:
-            print('created %s' % out_dir)
+            print("created %s" % out_dir)
 
-    subdir_paths = [os.path.join(api_modules, d)
-                    for d in os.listdir(api_modules)
-                    if not d.startswith('.')]
+    subdir_paths = [
+        os.path.join(api_modules, d)
+        for d in os.listdir(api_modules)
+        if not d.startswith(".")
+    ]
 
-    out_files = [os.path.join(out_dir, os.path.basename(d)) + '.md'
-                 for d in subdir_paths]
+    out_files = [
+        os.path.join(out_dir, os.path.basename(d)) + ".md" for d in subdir_paths
+    ]
 
     for sub_p, out_f in zip(subdir_paths, out_files):
-        module_paths = (os.path.join(sub_p, m)
-                        for m in os.listdir(sub_p)
-                        if not m.startswith('.'))
+        module_paths = (
+            os.path.join(sub_p, m) for m in os.listdir(sub_p) if not m.startswith(".")
+        )
 
         new_output = []
         if str_above_header:
             new_output.append(str_above_header)
         for p in sorted(module_paths):
-            with open(p, 'r') as r:
+            with open(p, "r") as r:
 
                 new_output.extend(r.readlines())
-                new_output.extend(['\n', '\n', '\n'])
+                new_output.extend(["\n", "\n", "\n"])
 
-        msg = ''
+        msg = ""
         if not os.path.isfile(out_f):
-            msg = 'created'
+            msg = "created"
 
-        if msg != 'created':
-            with open(out_f, 'r') as f:
+        if msg != "created":
+            with open(out_f, "r") as f:
                 prev = f.readlines()
             if prev != new_output:
-                msg = 'updated'
+                msg = "updated"
             else:
-                msg = 'skipped'
+                msg = "skipped"
 
-        if msg != 'skipped':
-            with open(out_f, 'w') as f:
-                f.write(''.join(new_output))
+        if msg != "skipped":
+            with open(out_f, "w") as f:
+                f.write("".join(new_output))
 
         if printlog:
-            print('%s %s' % (msg, out_f))
+            print("%s %s" % (msg, out_f))
 
 
 if __name__ == "__main__":
 
     import argparse
-    parser = argparse.ArgumentParser(
-        description='Convert docstring into a markdown API documentation.',
-        formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('-n', '--package_name',
-                        default='mlxtend',
-                        help='Name of the package')
-    parser.add_argument('-d', '--package_dir',
-                        default='../../mlxtend/',
-                        help="Path to the package's enclosing directory")
-    parser.add_argument('-o1', '--output_module_api',
-                        default='../docs/sources/api_modules',
-                        help=('Target directory for the module-level'
-                              ' API Markdown files'))
-    parser.add_argument('-o2', '--output_subpackage_api',
-                        default='../docs/sources/api_subpackages',
-                        help=('Target directory for the'
-                              ' subpackage-level API Markdown files'))
-    parser.add_argument('-c', '--clean',
-                        action='store_true',
-                        help='Remove previous API files')
-    parser.add_argument('-s', '--silent',
-                        action='store_true',
-                        help='Suppress log printed to the screen')
-    parser.add_argument('-v', '--version',
-                        action='version',
-                        version='v. 0.1')
-    parser.add_argument('--ignore_packages',
-                        default='',
-                        help='Ignores subpackages listed via this option.'
-                             ' For example, to ignore mlxtend.image,'
-                             ' type "mlxtend.image".'
-                             ' For multiple subpackages, separate them via,'
-                             ' commas. For example,'
-                             ' "mlxtend.image,mlxtend.plotting".')
+    parser = argparse.ArgumentParser(
+        description="Convert docstring into a markdown API documentation.",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.add_argument(
+        "-n", "--package_name", default="mlxtend", help="Name of the package"
+    )
+    parser.add_argument(
+        "-d",
+        "--package_dir",
+        default="../../mlxtend/",
+        help="Path to the package's enclosing directory",
+    )
+    parser.add_argument(
+        "-o1",
+        "--output_module_api",
+        default="../docs/sources/api_modules",
+        help=("Target directory for the module-level" " API Markdown files"),
+    )
+    parser.add_argument(
+        "-o2",
+        "--output_subpackage_api",
+        default="../docs/sources/api_subpackages",
+        help=("Target directory for the" " subpackage-level API Markdown files"),
+    )
+    parser.add_argument(
+        "-c", "--clean", action="store_true", help="Remove previous API files"
+    )
+    parser.add_argument(
+        "-s", "--silent", action="store_true", help="Suppress log printed to the screen"
+    )
+    parser.add_argument("-v", "--version", action="version", version="v. 0.1")
+    parser.add_argument(
+        "--ignore_packages",
+        default="",
+        help="Ignores subpackages listed via this option."
+        " For example, to ignore mlxtend.image,"
+        ' type "mlxtend.image".'
+        " For multiple subpackages, separate them via,"
+        " commas. For example,"
+        ' "mlxtend.image,mlxtend.plotting".',
+    )
 
     args = parser.parse_args()
 
     ignore_packages_set = set(args.ignore_packages.split(","))
 
     package = import_package(args.package_dir, args.package_name)
-    generate_api_docs(package=package,
-                      api_dir=args.output_module_api,
-                      clean=args.clean,
-                      ignore_packages=ignore_packages_set,
-                      printlog=not(args.silent))
-    summarize_methdods_and_functions(api_modules=args.output_module_api,
-                                     out_dir=args.output_subpackage_api,
-                                     printlog=not(args.silent),
-                                     clean=args.clean,
-                                     str_above_header=('mlxtend'
-                                                       ' version: %s \n' % (
-                                                        package.__version__)))
+    generate_api_docs(
+        package=package,
+        api_dir=args.output_module_api,
+        clean=args.clean,
+        ignore_packages=ignore_packages_set,
+        printlog=not (args.silent),
+    )
+    summarize_methdods_and_functions(
+        api_modules=args.output_module_api,
+        out_dir=args.output_subpackage_api,
+        printlog=not (args.silent),
+        clean=args.clean,
+        str_above_header=("mlxtend" " version: %s \n" % (package.__version__)),
+    )

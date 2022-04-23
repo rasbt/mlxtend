e3ae8d9412cc2f434db626736677dad4eb2e2536
rasbt <mail@sebastianraschka.com>
2022-04-23 08:13:07 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -11,46 +11,44 @@ import mlxtend
 VERSION = mlxtend.__version__
 PROJECT_ROOT = dirname(realpath(__file__))
 
-REQUIREMENTS_FILE = join(PROJECT_ROOT, 'requirements.txt')
+REQUIREMENTS_FILE = join(PROJECT_ROOT, "requirements.txt")
 
 with open(REQUIREMENTS_FILE) as f:
     install_reqs = f.read().splitlines()
 
-install_reqs.append('setuptools')
+install_reqs.append("setuptools")
 
 
-setup(name='mlxtend',
-      version=VERSION,
-      description='Machine Learning Library Extensions',
-      author='Sebastian Raschka',
-      author_email='mail@sebastianraschka.com',
-      url='https://github.com/rasbt/mlxtend',
-      packages=find_packages(),
-      package_data={'': ['LICENSE-BSD3.txt',
-                         'LICENSE-CC-BY.txt',
-                         'README.md',
-                         'requirements.txt']
-                    },
-      include_package_data=True,
-      install_requires=install_reqs,
-      extras_require={'testing': ['pytest'],
-                      'docs': ['mkdocs']},
-      license='BSD 3-Clause',
-      platforms='any',
-      classifiers=[
-             'License :: OSI Approved :: BSD License',
-             'Development Status :: 5 - Production/Stable',
-             'Operating System :: Microsoft :: Windows',
-             'Operating System :: POSIX',
-             'Operating System :: Unix',
-             'Operating System :: MacOS',
-             'Programming Language :: Python :: 3.7',
-             'Topic :: Scientific/Engineering',
-             'Topic :: Scientific/Engineering :: Artificial Intelligence',
-             'Topic :: Scientific/Engineering :: Information Analysis',
-             'Topic :: Scientific/Engineering :: Image Recognition',
-      ],
-      long_description="""
+setup(
+    name="mlxtend",
+    version=VERSION,
+    description="Machine Learning Library Extensions",
+    author="Sebastian Raschka",
+    author_email="mail@sebastianraschka.com",
+    url="https://github.com/rasbt/mlxtend",
+    packages=find_packages(),
+    package_data={
+        "": ["LICENSE-BSD3.txt", "LICENSE-CC-BY.txt", "README.md", "requirements.txt"]
+    },
+    include_package_data=True,
+    install_requires=install_reqs,
+    extras_require={"testing": ["pytest"], "docs": ["mkdocs"]},
+    license="BSD 3-Clause",
+    platforms="any",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+    ],
+    long_description="""
 
 A library of Python tools and extensions for data science.
 
@@ -67,4 +65,5 @@ This project is hosted at https://github.com/rasbt/mlxtend
 
 The documentation can be found at http://rasbt.github.io/mlxtend/
 
-""")
+""",
+)

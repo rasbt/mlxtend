d8478fdebd33887f4052d6e5b061eb79f66e818a
ninimama <nimasarajpoor@gmail.com>
2022-05-14 11:53:52 -0600

    revise warning message

--- mlxtend/frequent_patterns/fpcommon.py
+++ mlxtend/frequent_patterns/fpcommon.py
@@ -109,8 +109,9 @@ def valid_input_check(df):
     all_bools = df.dtypes.apply(pd.api.types.is_bool_dtype).all()
     if not all_bools:
         warnings.warn(
-            "Support for dataframe with non-bool type migth be discontinued in"
-            "the future. Use dataframe with type bool",
+            "DataFrames with non-bool types result in worse computational"
+            "performance and their support might be discontinued in the future."
+            "Please use a DataFrame with bool type",
             DeprecationWarning,
         )
         # Pandas is much slower than numpy, so use np.where on Numpy arrays
